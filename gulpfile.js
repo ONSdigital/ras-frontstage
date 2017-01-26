2474a21cfec8633e4c2f5fa20f97ff58d38c0637
Shane Edwards <shane.edwards9@gmail.com>
2017-01-26 15:42:03 +0000

    header changes

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,6 @@ let gulp = require('gulp'),
 	sass = require('gulp-sass'),
 	concat = require('gulp-concat');
 
-gulp.task('sass', () => {
-
-	return gulp.src('./ras-frontstage/main.sass')
-		.pipe(sass({indentedSyntax: false}).on('error', sass.logError))
-		.pipe(concat('site.min.css'))
-		.pipe(gulp.dest('./ras-frontstage/static'));
-
-});
-
 gulp.task('compile:sass', () => {
 
 	return gulp.src('./ras-frontstage/main.sass')

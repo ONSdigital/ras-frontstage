a2d6d162b2aa4908a2d55b0393492b3e1ed7f083
Nicholas Herriot <Nicholas.Herriot@gmail.com>
2017-03-21 13:59:49 +0000

    Adding all the code for ras-frontstage-backup to ras-frontstage.

---
+++ ras-frontstage/config.py
@@ -0,0 +1,75 @@
+"""
+This module hosts the config setup for our project
+"""
+
+import os
+
+
+# ENV VARIABLES BELOW, SET THESE ON YOUR TERMINAL
+# export APP_SETTINGS=config.Config
+# export FLASK_APP=app.py
+
+# Default values
+if "APP_SETTINGS" not in os.environ:
+    os.environ["APP_SETTINGS"] = "config.Config"
+
+
+class Config(object):
+    """
+    Base config class
+    """
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = 'this-really-needs-to-be-changed'
+    dbname = "ras_frontstage_backup"
+    SQLALCHEMY_DATABASE_URI = "postgresql://" + dbname + ":password@localhost:5431/postgres"
+
+
+class ProductionConfig(Config):
+    """
+    Production config class
+    """
+    DEBUG = False
+
+
+class StagingConfig(Config):
+    """
+    Staging config class
+    """
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class DevelopmentConfig(Config):
+    """
+    Development config class
+    """
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    """
+    Testing config class
+    """
+    TESTING = True
+
+
+class OAuthConfig(Config):
+    """
+    This class is used to configure OAuth2 parameters for the microservice.
+    This is temporary until an admin config feature
+    is added to allow manual config of the microservice
+    """
+    APP_ID = "399360140422360"  # This is an APP ID registered with the Facebook OAuth2
+
+    # App secret for a test registered Facebook OAuth2
+    APP_SECRET = "8daae4110e491db2c5067e5c89add2dd"
+
+    DISPLAY_NAME = "NoisyAtom"  # This is a test name registered with Facebook OAuth2
+    REDIRECT_ENDPOINT = ["http://104.236.14.123:8002/auth/callback",
+                         "http://104.236.14.123:8002/auth/callback.html"]
+
+    AUTHORIZATION_ENDPOINT = "https://www.facebook.com/dialog/oauth"  # Facebook Auth endpoint
+    TOKEN_ENDPOINT = "https://graph.facebook.com/oauth/access_token"  # Facebook token endpoint
---
+++ ras-frontstage/models.py
@@ -0,0 +1,85 @@
+"""
+This module contains the data model for the collection instrument
+"""
+
+import datetime
+
+from flask_wtf import Form
+from sqlalchemy import DateTime
+from sqlalchemy.dialects.postgresql import TEXT, JSON, UUID
+from werkzeug.security import generate_password_hash, check_password_hash
+from wtforms import TextField, DecimalField, SelectField, PasswordField
+from wtforms.validators import InputRequired, EqualTo
+
+from app import db
+
+
+class User(db.Model):
+    """User model."""
+
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100))
+    pwdhash = db.Column(db.String())
+    token = db.Column(db.String())
+    token_created_on = db.Column(DateTime)
+    token_duration = db.Column(db.Integer)
+    created_on = db.Column(DateTime, default=datetime.datetime.utcnow)
+
+    def __init__(self, username, password, token, token_created_on, token_duration, id=None):
+        """Init method."""
+        self.id = id
+        self.username = username
+        self.pwdhash = generate_password_hash(password)
+        self.token = token
+        self.token_created_on = token_created_on
+        self.token_duration = token_duration
+
+    def check_password(self, password):
+        """Method to check password validity."""
+        return check_password_hash(self.pwdhash, password)
+
+    def check_password_simple(self, password):
+        """Check password simplicity."""
+        if password == self.pwdhash:
+            print "Password checks out. Password in {}, password I have: {}".format(password, self.pwdhash)
+            return True
+        return False
+
+
+class UserScope(db.Model):
+    """Userscope model."""
+
+    __tablename__ = 'user_scopes'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer)
+    scope = db.Column(db.String(100))
+    created_on = db.Column(DateTime, default=datetime.datetime.utcnow)
+
+    def __init__(self, user_id, scope, id=None):
+        """Init method."""
+        self.id = id
+        self.user_id = user_id
+        self.scope = scope
+
+
+class RegistrationForm(Form):
+    """Registration form."""
+
+    username = TextField('Username', [InputRequired])
+    password = PasswordField( 'Password',[InputRequired(), EqualTo('confirm', message='Passwords must match')])
+    confirm = PasswordField('Confirm Password', [InputRequired()])
+
+
+class LoginForm(Form):
+    """Login form."""
+
+    username = TextField('Email Address', [InputRequired()])
+    password = PasswordField('Password', [InputRequired()])
+
+
+class SignIn(Form):
+    """Sign in form."""
+
+    username = TextField('Username', [InputRequired()])
+    password = PasswordField('Password', [InputRequired()])

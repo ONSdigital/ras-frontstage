9ec7eb7d6dde71b25b3d0aef485c22337e37f254
Andrew Millar <andrew.millar@qa.com>
2017-07-17 12:50:07 +0100

    disabling database in frontstage

--- tests/__init__.py
+++ tests/__init__.py
@@ -13,16 +13,16 @@ COLLECTION_INSTRUMENT_BUILD = 'ras_frontstage_backup_D0001_initial_build.sql'
 
 # Postgres = testing.postgresql.PostgresqlFactory(cache_initialized_db=True)
 # postgresql = Postgres()
-
-config.Config.SQLALCHEMY_DATABASE_URI = postgresql.url()
+#
+# config.Config.SQLALCHEMY_DATABASE_URI = postgresql.url()
 config.Config.CSRF_ENABLED = False
 config.Config.WTF_CSRF_ENABLED = False
 config.Config.OAUTHLIB_INSECURE_TRANSPORT = 1
-create_engine(postgresql.url())
-conn = psycopg2.connect(**postgresql.dsn())
-cursor = conn.cursor()
-
-sql_build = open(os.path.join(TEST_DIR, SQL_SCRIPTS + '/' + COLLECTION_INSTRUMENT_BUILD)).read()
-cursor.execute(sql_build)
-conn.commit()
-conn.close()
+# create_engine(postgresql.url())
+# conn = psycopg2.connect(**postgresql.dsn())
+# cursor = conn.cursor()
+#
+# sql_build = open(os.path.join(TEST_DIR, SQL_SCRIPTS + '/' + COLLECTION_INSTRUMENT_BUILD)).read()
+# cursor.execute(sql_build)
+# conn.commit()
+# conn.close()

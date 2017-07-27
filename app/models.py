1b4164ac6fcef03a2d7482ae31e5a86502ed97ab
Gareth Bult <gareth@bult.co.uk>
2017-07-27 09:43:46 +0100

    Attempt to cut down on conflicts

--- app/models.py
+++ app/models.py
@@ -13,7 +13,59 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from wtforms import HiddenField, PasswordField, StringField
 from wtforms.validators import InputRequired, EqualTo, Length, DataRequired, Email, ValidationError
 from app.config import Config
-from ons_ras_common import ons_env
+
+# db = SQLAlchemy()
+
+logger = wrap_logger(logging.getLogger(__name__))
+
+
+# class User(db.Model):
+#     """User model."""
+#
+#     __tablename__ = 'users'
+#     id = Column(Integer, primary_key=True)
+#     username = Column(String(100))
+#     pwdhash = Column(String())
+#     token = Column(String())
+#     token_created_on = Column(DateTime)
+#     token_duration = Column(Integer)
+#     created_on = Column(DateTime, default=datetime.datetime.utcnow)
+#
+#     def __init__(self, username, password, token, token_created_on, token_duration, id=None):
+#         """Init method."""
+#         self.id = id
+#         self.username = username
+#         self.pwdhash = generate_password_hash(password)
+#         self.token = token
+#         self.token_created_on = token_created_on
+#         self.token_duration = token_duration
+#
+#     def check_password(self, password):
+#         """Method to check password validity."""
+#         return check_password_hash(self.pwdhash, password)
+#
+#     def check_password_simple(self, password):
+#         """Check password simplicity."""
+#         if password == self.pwdhash:
+#             logger.debug("Password checks out. Password in {}, password I have: {}".format(password, self.pwdhash))
+#             return True
+#         return False
+#
+#
+# class UserScope(db.Model):
+#     """Userscope model."""
+#
+#     __tablename__ = 'user_scopes'
+#     id = Column(Integer, primary_key=True)
+#     user_id = Column(Integer)
+#     scope = Column(String(100))
+#     created_on = Column(DateTime, default=datetime.datetime.utcnow)
+#
+#     def __init__(self, user_id, scope, id=None):
+#         """Init method."""
+#         self.id = id
+#         self.user_id = user_id
+#         self.scope = scope
 
 
 class EnrolmentCodeForm(FlaskForm):
@@ -61,7 +113,7 @@ class RegistrationForm(FlaskForm):
 
     def validate_phone_number(form, field):
         try:
-            ons_env.logger.debug("Checking this is a valid GB Number")
+            logger.debug("Checking this is a valid GB Number")
             input_number = phonenumbers.parse(field.data, "GB")  # Tell the parser we are looking for a GB number
 
             if not (phonenumbers.is_possible_number(input_number)):
@@ -70,7 +122,7 @@ class RegistrationForm(FlaskForm):
             if not (phonenumbers.is_valid_number(input_number)):
                 raise ValidationError('Please use a valid UK number e.g. 01632 496 0018.')
         except NumberParseException:
-            ons_env.logger.debug(" There is a number parse exception in the phonenumber field")
+            logger.debug(" There is a number parse exception in the phonenumber field")
             raise ValidationError('This should be a valid UK number e.g. 01632 496 0018. ')
 
     def validate_password(form, field):
@@ -97,3 +149,25 @@ class EnrolmentCodeForm(FlaskForm):
     """
     enrolment_code = StringField('Enrolment Code', [InputRequired()])
 
+
+class ForgotPasswordForm(FlaskForm):
+    """
+    Forgot Password form.
+    """
+    email_address = StringField('Enter your email address',
+                                validators=[InputRequired("Email address is required"),
+                                            Email(message="Your email should be of the form 'myname@email.com' "),
+                                            Length(max=254,
+                                                   message='Your email must be less than 254 characters')])
+
+class ResetPasswordForm(FlaskForm):
+    """
+    Reset Password form.
+    """
+    password = PasswordField('New password',
+                             validators=[DataRequired("Password is required"),
+                                         EqualTo('password_confirm', message=Config.PASSWORD_MATCH_ERROR_TEXT),
+                                         Length(min=Config.PASSWORD_MIN_LENGTH,
+                                                max=Config.PASSWORD_MAX_LENGTH,
+                                                message=Config.PASSWORD_CRITERIA_ERROR_TEXT)])
+    password_confirm = PasswordField('Re-type new password')

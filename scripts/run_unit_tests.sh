280f7365f7bfcc988e36893a5d1094c50c59bfab
Andrew Millar <andrew.millar@qa.com>
2017-08-01 17:12:12 +0100

    fixing coverage testing

--- scripts/run_unit_tests.sh
+++ scripts/run_unit_tests.sh
@@ -1,3 +1,3 @@
 set -o pipefail
 
-py.test -s -v --cov=app --cov-report xml --ignore=node_modules --ignore=tests/selenium_scripts/
+py.test -s -v --cov=frontstage --cov-report xml --ignore=node_modules --ignore=tests/selenium_scripts/

24968692e6ce30b85be6786de94a71d83f8d00d4
Andrew Millar <andrew.millar@qa.com>
2017-08-04 10:50:18 +0100

    adding new 404 and 500 error page templates and endpoints

--- frontstage/views/errors.py
+++ frontstage/views/errors.py
@@ -14,3 +14,15 @@ def default_error_page():
     response = make_response(render_template('errors/error.html', _theme='default', data={"error": {"type": "failed"}}))
     response.set_cookie('authorization', value='', expires=0)
     return response
+
+
+@error_bp.route('/404', methods=['GET', 'POST'])
+def not_found_error_page():
+    response = make_response(render_template('errors/404-error.html', _theme='default', data={"error": {"type": "failed"}}))
+    return response
+
+
+@error_bp.route('/500', methods=['GET', 'POST'])
+def server_error_page():
+    response = make_response(render_template('errors/500-error.html', _theme='default', data={"error": {"type": "failed"}}))
+    return response

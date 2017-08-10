028c9f08f6113f2280efc7ac49d746d81600f39e
Richard Weeks <richard.weeks@ons.gov.uk>
2017-08-10 12:47:55 +0100

    Remove ras-common code.

--- frontstage/views/register.py
+++ frontstage/views/register.py
@@ -5,7 +5,6 @@ from os import getenv
 from flask import Blueprint, render_template, request, flash, redirect, url_for, abort
 from jose import JWTError
 from oauthlib.oauth2 import BackendApplicationClient, MissingTokenError
-from ons_ras_common import ons_env
 import requests
 from requests_oauthlib import OAuth2Session
 from structlog import wrap_logger
@@ -15,6 +14,7 @@ from frontstage.common.post_event import post_event
 from frontstage.jwt import encode
 from frontstage.models import RegistrationForm, EnrolmentCodeForm, RespondentStatus
 from frontstage.exceptions.exceptions import ExternalServiceError
+from frontstage.common.cryptographer import encrypt, decrypt
 
 
 logger = wrap_logger(logging.getLogger(__name__))
@@ -92,7 +92,7 @@ def register():
                        description='Access code authentication attempted')
 
             # Encrypt the enrolment code
-            coded_token = ons_env.crypt.encrypt(enrolment_code.encode()).decode()
+            coded_token = encrypt(enrolment_code.encode()).decode()
 
             return redirect(url_for('register_bp.register_confirm_organisation_survey',
                                     enrolment_code=coded_token,
@@ -118,7 +118,7 @@ def register_confirm_organisation_survey():
         encrypted_enrolment_code = request.form.get('enrolment_code')
 
     if encrypted_enrolment_code:
-        decrypted_enrolment_code = ons_env.crypt.decrypt(encrypted_enrolment_code.encode()).decode()
+        decrypted_enrolment_code = decrypt(encrypted_enrolment_code.encode()).decode()
     else:
         logger.error('Enrolment code not specified')
         return redirect(url_for('error_bp.default_error_page'))
@@ -205,7 +205,7 @@ def register_enter_your_details():
         encrypted_enrolment_code = request.form.get('enrolment_code')
 
     if encrypted_enrolment_code:
-        decrypted_enrolment_code = ons_env.crypt.decrypt(encrypted_enrolment_code.encode()).decode()
+        decrypted_enrolment_code = decrypt(encrypted_enrolment_code.encode()).decode()
     else:
         logger.error('Enrolment code not specified')
         return redirect(url_for('error_bp.default_error_page'))

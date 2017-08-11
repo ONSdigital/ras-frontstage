df0163c482c138b8491a1e405d70c39bbb2e3cd8
Richard Weeks <richard.weeks@ons.gov.uk>
2017-08-11 14:26:12 +0100

    Add crypto test.

--- tests/test_cryptographer.py
+++ tests/test_cryptographer.py
@@ -1,15 +1,19 @@
-# import unittest
-#
-# from frontstage.common.cryptographer import Cryptographer
-#
-# class CryptoTestClass(unittest.TestCase):
-#
-#     def setUp(self):
-#         cryptographer = Cryptographer()
-#
-#     def test_encrypt(self):
-#         test_str = "Hello"
-#         self.assertEqual(cryptographer.encrypt(test_str), 302)
-#
-# if __name__ == '__main__':
-#     unittest.main()
+import unittest
+
+from frontstage.common.cryptographer import Cryptographer
+
+cryptographer = Cryptographer()
+
+class CryptoTestClass(unittest.TestCase):
+
+
+
+    def test_encrypt(self):
+        test_str = b'Hello'
+        encrypted_str = cryptographer.encrypt(test_str)
+        decrypted_str = cryptographer.decrypt(encrypted_str)
+        self.assertTrue(test_str==decrypted_str)
+
+
+if __name__ == '__main__':
+    unittest.main()

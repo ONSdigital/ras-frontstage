3e85a7fca390bbf3b384a2db2e79888d2d55f903
Andrew Millar <andrew.millar@qa.com>
2017-08-18 12:27:42 +0100

    removing logs from oauth pacakge

--- frontstage/logger_config.py
+++ frontstage/logger_config.py
@@ -1,22 +1,39 @@
 import logging
 import os
 
+from structlog import configure
+from structlog.stdlib import add_log_level, filter_by_level
+from structlog.processors import JSONRenderer, TimeStamper
+
 
 def logger_initial_config(service_name=None,
                           log_level=None,
                           logger_format=None,
                           logger_date_format=None):
-    '''Set initial logger config'''
 
+    if not logger_date_format:
+        logger_date_format = os.getenv('LOGGING_DATE_FORMAT', "%Y-%m-%dT%H:%M%s")
     if not log_level:
         log_level = os.getenv('SMS_LOG_LEVEL', 'INFO')
     if not logger_format:
-        logger_format = (
-            "level='%(levelname)s' service='{}' created=%(asctime)s.%(msecs)06dZ | "
-            "%(message)s file='%(name)s.%(funcName)s' line_no=%(lineno)d").format(service_name)
-    if not logger_date_format:
-        logger_date_format = os.getenv('LOGGING_DATE_FORMAT', "%Y-%m-%dT%H:%M%s")
+        logger_format = "%(message)s"
+    if not service_name:
+        service_name = os.getenv('NAME', 'ras-frontstage')
+
+    def add_service(logger, method_name, event_dict):
+        """
+        Add the service name to the event dict.
+        """
+        event_dict['service'] = service_name
+        return event_dict
 
     logging.basicConfig(level=log_level,
-                        format=logger_format,
-                        datefmt=logger_date_format)
+                        format=logger_format)
+    configure(processors=[add_log_level,
+                          filter_by_level,
+                          add_service,
+                          TimeStamper(fmt=logger_date_format, utc=True, key="created_at"),
+                          JSONRenderer(indent=1)])
+    oauth_log = logging.getLogger("requests_oauthlib")
+    oauth_log.addHandler(logging.NullHandler())
+    oauth_log.propagate = False

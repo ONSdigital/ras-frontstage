74d90a8614858fd88c23c112363ac9b867a5814c
dcdarrell9 <dcdarrell9@users.noreply.github.com>
2017-08-29 10:57:20 +0100

    changed port number

--- frontstage/__init__.py
+++ frontstage/__init__.py
@@ -1,7 +1,5 @@
 import logging
 import os
-import frontstage.views
-import frontstage.error_handlers
 
 from flask import Flask
 from structlog import wrap_logger
@@ -28,6 +26,5 @@ if app.config['DEBUG']:
 logger_initial_config(service_name='ras-frontstage', log_level=log_level)
 logger = wrap_logger(logging.getLogger(__name__))
 
-
-
-
+import frontstage.views
+import frontstage.error_handlers

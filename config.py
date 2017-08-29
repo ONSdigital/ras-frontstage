1dbf4f744c6cb0c2fe1378b97dfe13a2e240291c
Andrew Millar <andrew.millar@qa.com>
2017-08-29 15:10:35 +0100

    initial config tidy up

--- config.py
+++ config.py
@@ -1,34 +1,4 @@
-"""
-This module hosts the config setup for our project
-"""
-
 import os
-
-# ENV VARIABLES BELOW, SET THESE ON YOUR TERMINAL
-# export APP_SETTINGS=config.Config
-# export FLASK_APP=application.py
-# export OAUTHLIB_INSECURE_TRANSPORT=1
-
-# Environment variables for running locally using the server.js Node server in ras-backstage-ui)
-# To use these stubs:
-#   git clone ras-backstage-ui
-#   npm install
-#   node server.js to run the app on localhost:8050
-#
-# And set these environment variables on the ras-frontstage terminal:
-# export API_GATEWAY_CASE_URL=http://localhost:8050/api/cases/
-# export API_GATEWAY_COLLECTION_INSTRUMENT_URL=http://localhost:8050/api/collection-instruments/
-# export API_GATEWAY_SURVEYS_URL=http://localhost:8050/api/surveys/
-# export API_GATEWAY_AGGREGATED_SURVEYS_URL=http://localhost:8050/api/my-surveys/
-# export API_GATEWAY_PARTY_URL=http://localhost:8050/api/party-api/
-# export API_GATEWAY_IAC_URL=http://localhost:8050/api/iacs/
-
-# export ONS_OAUTH_SERVER=ras-django-int.apps.devtest.onsclofo.uk
-# export ONS_OAUTH_SERVER=ras-django-ci.apps.devtest.onsclofo.uk
-# Log is as testuser@email.com , password
-# OR
-# Log in using your account on:
-# export ONS_OAUTH_SERVER=ons-oauth2.cfapps.io
 
 
 # Default values
@@ -37,21 +7,16 @@ if "APP_SETTINGS" not in os.environ:
 
 
 class Config(object):
-    """
-    Base config class
-    """
-    NAME = os.getenv('NAME', 'ras-frontstage')
-    VERSION = os.getenv('VERSION', '0.2.0')
-
-    RAS_FS_CRYPTO_KEY = 'ONS_DUMMY_KEY'
     DEBUG = False
     TESTING = False
-    CSRF_ENABLED = False
-    # WTF_CSRF_CHECK_DEFAULT = False
-    WTF_CSRF_ENABLED = False
-    SECRET_KEY = 'this-really-needs-to-be-changed'
+    NAME = os.getenv('NAME', 'ras-frontstage')
+    VERSION = os.getenv('VERSION', '0.2.0')
     dbname = "ras_frontstage_backup"
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI', 'postgresql://ras_frontstage_backup:password@localhost:5431/postgres')
+    SECRET_KEY = 'this-really-needs-to-be-changed'
+    RAS_FS_CRYPTO_KEY = 'ONS_DUMMY_KEY'
+    CSRF_ENABLED = False
+    WTF_CSRF_ENABLED = False
     VALIDATE_JWT = os.environ.get('VALIDATE_JWT', True)
     GOOGLE_ANALYTICS = os.getenv('GOOGLE_ANALYTICS', None)
 
@@ -62,38 +27,8 @@ class Config(object):
     ONS_AUTHORIZATION_ENDPOINT = os.environ.get('ONS_AUTHORIZATION_ENDPOINT', '/web/authorize/')
     ONS_TOKEN_ENDPOINT = os.environ.get('ONS_TOKEN_ENDPOINT', '/api/v1/tokens/')
     ONS_ADMIN_ENDPOINT = os.environ.get('ONS_ADMIN_ENDPOINT', '/api/account/create')
-    
-    # Cloud Foundry config
 
-    # TODO - Define default CF endpoints
-    API_GATEWAY_CASE_URL = os.environ.get('API_GATEWAY_CASE_URL',
-                                          'http://localhost:8050/api/cases/')
-
-    # TODO - Define default CF endpoints
-    API_GATEWAY_COLLECTION_EXERCISE_URL = os.environ.get('API_GATEWAY_COLLECTION_EXERCISE_URL',
-                                                         'http://localhost:8050/api/collectionexercises/')
-
-    API_GATEWAY_COLLECTION_INSTRUMENT_URL = os.environ.get('API_GATEWAY_COLLECTION_INSTRUMENT_URL',
-                                                           'http://ras-api-gateway-int.apps.devtest.onsclofo.uk:80/collection-instrument-api/1.0.2/')
-    API_GATEWAY_SURVEYS_URL = os.environ.get('API_GATEWAY_SURVEYS_URL',
-                                             'http://ras-api-gateway-int.apps.devtest.onsclofo.uk:80/surveys/')
-    API_GATEWAY_AGGREGATED_SURVEYS_URL = os.environ.get('API_GATEWAY_AGGREGATED_SURVEYS_URL',
-                                                        'http://ras-api-gateway-int.apps.devtest.onsclofo.uk/api/1.0.0/surveys/')
-    API_GATEWAY_PARTY_URL = os.environ.get('API_GATEWAY_PARTY_URL',
-                                           'http://ras-api-gateway-int.apps.devtest.onsclofo.uk/party-api/v1/')
-    API_GATEWAY_IAC_URL = os.environ.get('API_GATEWAY_IAC_URL',
-                                         'http://ras-api-gateway-int.apps.devtest.onsclofo.uk/api/1.0.0/iacs/')
-    
-    PASSWORD_MATCH_ERROR_TEXT = 'Your passwords do not match'
-    PASSWORD_CRITERIA_ERROR_TEXT = 'Your password doesn\'t meet the requirements'
-    PASSWORD_MIN_LENGTH = 8
-    PASSWORD_MAX_LENGTH = 160
-
-    #
-    #   These settings are to support the new post_event routine which has been ported
-    #   into ras-frontstage from ras-common. The style of environment variable over-ride
-    #   has been taken from JB's new common code implementation.
-    #
+    # Configurations for external services
     RM_CASE_SERVICE_HOST = os.getenv('RM_CASE_SERVICE_HOST', 'localhost')
     RM_CASE_SERVICE_PORT = os.getenv('RM_CASE_SERVICE_PORT', 8171)
     RM_CASE_SERVICE_PROTOCOL = os.getenv('RM_CASE_SERVICE_PROTOCOL', 'http')
@@ -142,22 +77,24 @@ class Config(object):
                                                       RAS_SECURE_MESSAGE_SERVICE_HOST,
                                                       RAS_SECURE_MESSAGE_SERVICE_PORT)
 
+    # Specific external service endpoints
     RAS_AGGREGATOR_TODO = '{}api/1.0.0/surveys/todo/{}'
     RAS_CI_UPLOAD = '{}collection-instrument-api/1.0.2/survey_responses/{}'
     RAS_CI_GET = '{}collection-instrument-api/1.0.2/collectioninstrument/id/{}'
     RAS_CI_DOWNLOAD = '{}collection-instrument-api/1.0.2/download/{}'
     RM_CASE_GET = '{}cases/{}'
     RAS_PARTY_GET_BY_BUSINESS = '{}party-api/v1/businesses/id/{}'
+    RAS_PARTY_GET_BY_EMAIL = '{}party-api/v1/respondents/email/{}'
     RAS_PARTY_GET_BY_RESPONDENT = '{}party-api/v1/respondents/id/{}'
-    RM_COLLECTION_EXERCISES_GET = '{}collectionexercises/{}'
-    RM_SURVEY_GET = '{}surveys/{}'
     RAS_PARTY_POST_RESPONDENTS = '{}party-api/v1/respondents'
     RAS_PARTY_VERIFY_EMAIL = '{}party-api/v1/emailverification/{}'
+    RM_COLLECTION_EXERCISES_GET = '{}collectionexercises/{}'
+    RM_SURVEY_GET = '{}surveys/{}'
     RM_IAC_GET = '{}iacs/{}'
     RM_CASE_GET_BY_PARTY = '{}cases/partyid/{}'
     RM_CASE_GET_BY_IAC = '{}cases/iac/{}'
-    RAS_PARTY_GET_BY_EMAIL = '{}party-api/v1/respondents/email/{}'
 
+    # Secure messaging endpoints
     MESSAGE_LIMIT = os.getenv('MESSAGE_LIMIT', 1000)
     CREATE_MESSAGE_API_URL = RAS_SECURE_MESSAGE_SERVICE + 'message/send'
     MESSAGES_API_URL = RAS_SECURE_MESSAGE_SERVICE + 'messages?limit={}'.format(MESSAGE_LIMIT)
@@ -167,20 +104,10 @@ class Config(object):
     DRAFT_GET_API_URL = RAS_SECURE_MESSAGE_SERVICE + 'draft/{}'
     DRAFT_PUT_API_URL = RAS_SECURE_MESSAGE_SERVICE + 'draft/{}/modify'
 
-
-class ProductionConfig(Config):
-    """
-    Production config class
-    """
-    DEBUG = False
-
-
-class StagingConfig(Config):
-    """
-    Staging config class
-    """
-    DEVELOPMENT = True
-    DEBUG = True
+    PASSWORD_MATCH_ERROR_TEXT = 'Your passwords do not match'
+    PASSWORD_CRITERIA_ERROR_TEXT = 'Your password doesn\'t meet the requirements'
+    PASSWORD_MIN_LENGTH = 8
+    PASSWORD_MAX_LENGTH = 160
 
 
 class DevelopmentConfig(Config):

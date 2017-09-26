e55ca315ec7763edb6f94a3485ba1754458fac2f
nicprice <po7vifNSvI2Y>
2017-09-26 17:52:37 +0100

    Changes to the header to add the Help button and information
    Including only hiding the info and showing the button if javascript is running.
    
    Still need to remove transition from the initial hide on page load.
    
    Also need to improve how it looks on small screen when signed in.

---
+++ frontstage/assets/js/app/modules/help.js
@@ -0,0 +1,34 @@
+import domready from '../../../../../common/assets/js/app/modules/domready';
+
+domready(() => {
+  const btn = document.querySelector('.js-help-btn')
+  const help = document.querySelector('.js-help-body')
+  const classClosedInit = 'is-closed--init'
+  const classClosed = 'is-closed'
+
+  let openedByClick = false
+
+  if (help === null || btn === null) {
+    return false
+  }
+
+  help.classList.add(classClosed);
+  btn.classList.remove('u-hidden');
+
+  help.addEventListener('focus', e => {
+    help.classList.remove(classClosed)
+  }, true)
+
+  help.addEventListener('blur', e => {
+    if (!openedByClick) {
+      help.classList.add(classClosed)
+    }
+  }, true)
+
+  btn.classList.remove('u-hidden')
+  btn.addEventListener('click', e => {
+    e.preventDefault()
+    openedByClick = !openedByClick
+    help.classList.toggle(classClosed)
+  })
+})

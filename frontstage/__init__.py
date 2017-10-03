c8fcf9d7b3dd9c57d50ea68b5ff3b6e220254677
Tejas Patel <tejas.patel@ons.gov.uk>
2017-10-03 15:42:39 +0100

    [no subject] should show if no subject

--- frontstage/__init__.py
+++ frontstage/__init__.py
@@ -8,6 +8,7 @@ from structlog import wrap_logger
 from frontstage.exceptions.exceptions import MissingEnvironmentVariable
 from frontstage.filters.case_status_filter import case_status_filter
 from frontstage.filters.file_size_filter import file_size_filter
+from frontstage.filters.subject_filter import subject_filter
 from frontstage.logger_config import logger_initial_config
 
 
@@ -31,6 +32,7 @@ csrf = CSRFProtect(app)
 
 app.jinja_env.filters['case_status_filter'] = case_status_filter
 app.jinja_env.filters['file_size_filter'] = file_size_filter
+app.jinja_env.filters['subject_filter'] = subject_filter
 
 
 @app.after_request
---
+++ frontstage/filters/subject_filter.py
@@ -0,0 +1,8 @@
+def subject_filter(subject):
+    """
+    Filter to format subject if subject is empty or has only spaces.
+    """
+    if subject is None or len(subject.strip()) < 1:
+        subject = '[no subject]'
+
+    return subject

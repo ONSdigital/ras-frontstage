6da2a7deefa3d98018fcbac18c6f450b4fd8dbe3
nicprice <po7vifNSvI2Y>
2017-10-03 10:53:03 +0100

    Latest updates
    Just need to re-check what it looks like when signed in

--- frontstage/assets/js/app/modules/help.js
+++ frontstage/assets/js/app/modules/help.js
@@ -1,5 +1,38 @@
 import domready from '../../../../../common/assets/js/app/modules/domready';
 
+domready(function () {
+  var btn = document.querySelector('.js-help-btn');
+  var help = document.querySelector('.js-help-body');
+  var classClosed = 'is-closed';
+
+  var openedByClick = false;
+
+  if (help === null || btn === null) {
+    return false;
+  }
+
+  help.classList.add(classClosed);
+  btn.classList.remove('u-vh');
+
+  help.addEventListener('focus', function (e) {
+    help.classList.remove(classClosed);
+  }, true);
+
+  help.addEventListener('blur', function (e) {
+    if (!openedByClick) {
+      help.classList.add(classClosed);
+    }
+  }, true);
+
+  btn.addEventListener('click', function (e) {
+    e.preventDefault();
+    openedByClick = !openedByClick;
+    help.classList.toggle(classClosed);
+  });
+});
+
+
+/*
 domready(() => {
   const btn = document.querySelector('.js-help-btn');
   const help = document.querySelector('.js-help-body');
@@ -12,7 +45,7 @@ domready(() => {
     return false;
   }
 
-  help.classList.add(classClosedInit);
+  //help.classList.add(classClosedInit);
   help.classList.add(classClosed);
   btn.classList.remove('u-vh');
 
@@ -32,3 +65,4 @@ domready(() => {
     help.classList.toggle(classClosed);
   });
 });
+*/

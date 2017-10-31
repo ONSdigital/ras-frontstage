5c7985a32c054e9a5b033287995984d2a9b19548
andrewmil <Andrew.Millar@qa.com>
2017-10-31 09:28:53 +0000

    Tbl087/moving more secure message requests to api no error codes (#229)
    * moving secure message calls to frontstage api
    
    * updating tests for new api unctionality
    
    * addressing PR comments
    
    * adding default unread messages total
    
    * removing error codes
    
    * fixing tests

--- frontstage/exceptions/exceptions.py
+++ frontstage/exceptions/exceptions.py
@@ -1,7 +1,15 @@
 import sys
 
 
+class ApiError(Exception):
+
+    def __init__(self, response):
+        self.url = response.url
+        self.status_code = response.status_code
+
+
 class ExternalServiceError(Exception):
+
     def __init__(self, response):
         self.url = response.url
         self.status_code = response.status_code
--- frontstage/views/secure_messaging.py
+++ frontstage/views/secure_messaging.py
@@ -1,12 +1,11 @@
 import logging
 
-from flask import Blueprint, json, redirect, render_template, request, session, url_for
+from flask import Blueprint, json, render_template, request
 from frontstage.common.authorisation import jwt_authorization
-import requests
 from structlog import wrap_logger
 
 from frontstage import app
-from frontstage.exceptions.exceptions import ExternalServiceError
+from frontstage.exceptions.exceptions import ApiError
 from frontstage.common.api_call import api_call
 
 
@@ -24,41 +23,11 @@ def create_message(session):
     """Handles sending of new message"""
     if request.method == 'POST':
         party_id = session['party_id']
-        loggerb = logger.bind(party_id=party_id)
+        is_draft = True if request.form['submit'] == 'Save draft' else False
+        return send_message(party_id, is_draft)
 
-        # Get user details using party_id
-        collection_case = get_collection_case(party_id)
-        if collection_case is None:
-            return redirect(url_for('error_bp.default_error_page'))
-        ru_id = get_party_ru_id(party_id)
-        if ru_id is None:
-            return redirect(url_for('error_bp.default_error_page'))
-        survey_name = get_survey_id(party_id)
-        if survey_name is None:
-            return redirect(url_for('error_bp.default_error_page'))
-
-        # Create message object to send to secure-message service
-        data = {'msg_to': ['BRES'],
-                'msg_from': session['party_id'],
-                'subject': request.form['secure-message-subject'],
-                'body': request.form['secure-message-body'],
-                'thread_id': request.form['secure-message-thread-id'],
-                'collection_case': collection_case,
-                'ru_id': ru_id,
-                'survey': survey_name}
-
-        # Set msg_id if message already saved as draft
-        if "msg_id" in request.form:
-            data["msg_id"] = request.form['msg_id']
-            loggerb = loggerb.bind(message_id=data["msg_id"])
-
-        if request.form['submit'] == 'Send':
-            return send_message(data, loggerb)
-
-        elif request.form['submit'] == 'Save draft':
-            return save_draft(data, loggerb)
-
-    return render_template('secure-messages/secure-messages-view.html', _theme='default', message={})
+    elif request.method == 'GET':
+        return render_template('secure-messages/secure-messages-view.html', _theme='default', message={})
 
 
 @secure_message_bp.route('/<label>/<message_id>', methods=['GET'])
@@ -66,27 +35,11 @@ def create_message(session):
 def message_get(session, label, message_id):
     """Get message"""
     party_id = session['party_id']
-    loggerb = logger.bind(message_id=message_id, party_id=party_id, label=label)
-    message = get_message(message_id, label, loggerb)
-
-    # If message is a draft try and get the last message from its thread
-    if label == 'DRAFT':
-        draft = message
-        thread_id = draft.get('thread_id')
-        if thread_id != draft['msg_id']:
-            message = get_thread_message(thread_id, loggerb, party_id)
-        else:
-            message = None
-    else:
-        draft = {}
-
-    if label == 'UNREAD':
-        remove_unread_label(message_id, loggerb)
-
+    message_json = get_message(message_id, label, party_id)
     return render_template('secure-messages/secure-messages-view.html',
                            _theme='default',
-                           message=message,
-                           draft=draft,
+                           message=message_json['message'],
+                           draft=message_json['draft'],
                            label=label)
 
 
@@ -95,195 +48,87 @@ def message_get(session, label, message_id):
 @jwt_authorization(request)
 def messages_get(session, label="INBOX"):
     """Gets users messages"""
-    party_id = session['party_id']
-    loggerb = logger.bind(party_id=party_id, label=label)
-    messages = get_messages_list(label, loggerb)
-    unread_msg_total = messages.get('total')
+    messages_list = get_messages_list(label)
+    messages = messages_list['messages']
+    unread_msg_total = messages_list.get('unread_messages_total', 0)
     return render_template('secure-messages/secure-messages.html', _theme='default', messages=messages['messages'],
                            links=messages['_links'], label=label, total=unread_msg_total)
 
 
-def get_party_ru_id(party_id):
-    url = app.config['RAS_PARTY_GET_BY_RESPONDENT'].format(app.config['RAS_PARTY_SERVICE'], party_id)
-    logger.debug('Retrieving ru_id', party_id=party_id)
-    party_response = requests.get(url, auth=app.config['BASIC_AUTH'])
-    if party_response.status_code == 404:
-        logger.warning('No respondent found in party service', party_id=party_id)
-        return None
-    elif party_response.status_code != 200:
-        logger.error('Failed to retrieve ru_id')
-        raise ExternalServiceError(party_response)
-    party_response_json = party_response.json()
-    associations = party_response_json.get('associations')
-    if associations:
-        ru_id = associations[0].get('partyId')
-        logger.debug('Successfully received ru_id', ru_id=ru_id)
-    else:
-        logger.error('Respondent has no associations', party_id=party_id)
-        ru_id = None
-    return ru_id
+def get_messages_list(label):
+    logger.debug('Attempting to retrieve messages', label=label)
 
-
-def get_collection_case(party_id):
-    url = app.config['RM_CASE_GET_BY_PARTY'].format(app.config['RM_CASE_SERVICE'], party_id, "")
-    logger.debug('Retrieving collection case id')
-    collection_response = requests.get(url, auth=app.config['BASIC_AUTH'])
-    if collection_response.status_code == 204:
-        logger.warning('No case found')
-        return None
-    elif collection_response.status_code != 200:
-        logger.error('Failed to retrieve collection case id')
-        raise ExternalServiceError(collection_response)
-    collection_response_json = collection_response.json()
-    case_id = collection_response_json[0].get('id')
-    logger.debug('Successfully received collection case', case_id=case_id)
-    return case_id
-
-
-def get_survey_id(party_id):
-    url = app.config['RAS_PARTY_GET_BY_RESPONDENT'].format(app.config['RAS_PARTY_SERVICE'], party_id)
-    logger.debug('Retrieving survey id', party_id=party_id)
-    survey_response = requests.get(url, auth=app.config['BASIC_AUTH'])
-    if survey_response == 404:
-        logger.warning('No respondent found in party service', party_id=party_id)
-        return None
-    elif survey_response.status_code != 200:
-        logger.error('Failed to retrieve survey id', party_id=party_id)
-        raise ExternalServiceError(survey_response)
-    survey_response_json = survey_response.json()
-    associations = survey_response_json.get('associations')
-    if associations:
-        survey_name = associations[0].get('enrolments')[0].get('surveyId')
-    else:
-        logger.error('Respondent has no associations', party_id=party_id)
-        survey_name = None
-    logger.debug('Successfully received survey name', survey_id=survey_name, party_id=party_id)
-    return survey_name
-
-
-def get_messages_list(label, logger):
-    logger.debug('Attempting to retrieve messages')
+    # Form api request
     headers = {"Authorization": request.cookies['authorization']}
     endpoint = app.config['GET_MESSAGES_URL']
     parameters = {"label": label} if label else {}
     response = api_call('GET', endpoint, parameters=parameters, headers=headers)
 
     if response.status_code != 200:
-        logger.error('Error retrieving user messages')
-        raise ExternalServiceError(response)
+        logger.error('Failed to retrieve messages list', label=label)
+        raise ApiError(response)
 
     messages_list = json.loads(response.text)
-    logger.debug('Retrieved messages list successfully')
+    logger.debug('Retrieved messages list successfully', label=label)
     return messages_list
 
 
-def get_message(message_id, label, logger):
+def get_message(message_id, label, party_id):
+    logger.debug('Attempting to retrieve message', message_id=message_id, party_id=party_id)
+
+    # Form api request
     headers = {"Authorization": request.cookies['authorization']}
-    logger.debug('Attempting to retrieve message')
-    if label == 'DRAFT':
-        url = app.config['DRAFT_GET_API_URL'].format(message_id)
-    else:
-        url = app.config['MESSAGE_GET_URL'].format(message_id)
-    response = requests.get(url, headers=headers)
+    endpoint = app.config['GET_MESSAGE_URL']
+    parameters = {"message_id": message_id, "label": label, "party_id": party_id}
+    response = api_call('GET', endpoint, parameters=parameters, headers=headers)
 
     if response.status_code != 200:
-        logger.error('Failed to retrieve message')
-        raise ExternalServiceError(response)
+        logger.error('Failed to retrieve message', message_id=message_id, party_id=party_id)
+        raise ApiError(response)
 
     message = json.loads(response.text)
-    logger.debug('Retrieved message successfully')
+    logger.debug('Retrieved message successfully', message_id=message_id, party_id=party_id)
     return message
 
 
-def get_thread_message(thread_id, logger, party_id):
+def send_message(party_id, is_draft):
+    logger.debug('Attempting to send message', party_id=party_id)
+
+    # Form api request
     headers = {"Authorization": request.cookies['authorization']}
-    logger.debug('Attempting to retrieve message from thread', thread_id=thread_id)
-    url = app.config['THREAD_GET_API_URL'].format(thread_id)
-    response = requests.get(url, headers=headers)
+    endpoint = app.config['SEND_MESSAGE_URL']
+    message_json = {
+        'msg_from': party_id,
+        'subject': request.form['secure-message-subject'],
+        'body': request.form['secure-message-body'],
+        'thread_id': request.form['secure-message-thread-id']
+    }
+    # If message has previously been saved as a draft add through the message id
+    if "msg_id" in request.form:
+        message_json["msg_id"] = request.form['msg_id']
+    response = api_call('POST', endpoint, parameters={"is_draft": is_draft}, json=message_json, headers=headers)
 
     if response.status_code != 200:
-        logger.error('Failed to retrieve thread', thread_id=thread_id)
-        raise ExternalServiceError(response)
+        logger.debug('Failed to send message')
+        raise ApiError(response)
 
-    # Search through the messages in the thread and set message to the last message the user received
-    thread = json.loads(response.text)
-    for thread_message in thread['messages']:
-        if thread_message['@msg_from']['id'] != party_id:
-            message = thread_message
-            break
-    else:
-        message = None
-        logger.error('No message found in thread not belonging to the user')
+    sent_message = json.loads(response.text)
 
-    logger.debug('Retrieved message from thread successfully', thread_id=thread_id)
-    return message
+    # If form errors are returned render them
+    if sent_message.get('error'):
+        logger.debug('Form submitted with errors', party_id=party_id)
+        message = sent_message.get('error', {}).get('data', {}).get('thread_message')
+        errors = sent_message['error']['data']['form_errors']
+        return render_template('secure-messages/secure-messages-view.html',
+                               _theme='default',
+                               message=message,
+                               draft=message_json,
+                               errors=errors)
 
+    # If draft was saved render the saved draft
+    if is_draft:
+        logger.info('Draft sent successfully', message_id=sent_message['msg_id'], party_id=party_id)
+        return message_get('DRAFT', sent_message['msg_id'])
 
-def process_form_errors(response, data, logger):
-    logger.warning("Bad request to secure message service")
-    errors = json.loads(response.content)
-
-    if data.get('thread_id'):
-        message = get_thread_message(data['thread_id'], logger, data['msg_from'])
-    else:
-        message = {}
-
-    return render_template('secure-messages/secure-messages-view.html',
-                           _theme='default',
-                           message=message,
-                           draft=data,
-                           errors=errors)
-
-
-def save_draft(data, logger):
-    headers = {"Authorization": request.cookies['authorization']}
-    logger.debug("Attempting to save draft")
-    if data.get('msg_id'):
-        url = app.config['DRAFT_PUT_API_URL'].format(data['msg_id'])
-        response = requests.put(url, json=data, headers=headers)
-    else:
-        url = app.config['DRAFT_SAVE_API_URL']
-        response = requests.post(url, json=data, headers=headers)
-
-    if response.status_code == 400:
-        logger.debug('Form submitted with errors')
-        return process_form_errors(response, data, logger)
-    elif response.status_code != 200 and response.status_code != 201:
-        logger.error("Failed to save draft")
-        raise ExternalServiceError(response)
-
-    response_data = json.loads(response.text)
-    logger.info('Saved draft successfully', message_id=response_data['msg_id'])
-    return message_get(label='DRAFT', message_id=response_data['msg_id'])
-
-
-def send_message(data, logger):
-    headers = {"Authorization": request.cookies['authorization']}
-    logger.debug("Attempting to send message")
-    url = app.config['CREATE_MESSAGE_API_URL']
-    response = requests.post(url, json=data, headers=headers)
-
-    if response.status_code == 400:
-        logger.debug('Form submitted with errors')
-        return process_form_errors(response, data, logger)
-    elif response.status_code != 201 and response.status_code != 400:
-        logger.error('Failed to create message')
-        raise ExternalServiceError(response)
-
-    message = json.loads(response.text)
-    logger.info('Secure Message sent successfully', message_id=message['msg_id'])
+    logger.info('Secure message sent successfully', message_id=sent_message['msg_id'], party_id=party_id)
     return render_template('secure-messages/message-success-temp.html', _theme='default')
-
-
-def remove_unread_label(message_id, logger):
-    headers = {"Authorization": request.cookies['authorization']}
-    logger.debug('Attempting to remove unread label')
-    data = {"label": 'UNREAD', "action": 'remove'}
-    url = app.config['MESSAGE_MODIFY_URL'].format(message_id)
-    response = requests.put(url, json=data, headers=headers)
-
-    if response.status_code != 200:
-        logger.error("Failed to remove unread label")
-        return False
-    logger.debug("Removed unread label")
-    return True

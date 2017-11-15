3d8a789e313f18640590089862596d44894cd996
Andrew Millar <andrew.millar@qa.com>
2017-11-15 15:58:01 +0000

    Remove unnecessary comments in contact_us/cookies

--- frontstage/views/contact_us.py
+++ frontstage/views/contact_us.py
@@ -3,12 +3,12 @@ import logging
 from flask import Blueprint, render_template
 from structlog import wrap_logger
 
+
 logger = wrap_logger(logging.getLogger(__name__))
 
 contact_us_bp = Blueprint('contact_us_bp', __name__, static_folder='static', template_folder='templates')
 
 
-# ===== Cookies Policy =====
-@contact_us_bp.route('/', methods=['GET', 'POST'])
+@contact_us_bp.route('/', methods=['GET'])
 def contact_us():
     return render_template('contact-us.html', _theme='default')
--- frontstage/views/cookies_privacy.py
+++ frontstage/views/cookies_privacy.py
@@ -3,12 +3,12 @@ import logging
 from flask import Blueprint, render_template
 from structlog import wrap_logger
 
+
 logger = wrap_logger(logging.getLogger(__name__))
 
 cookies_privacy_bp = Blueprint('cookies_privacy_bp', __name__, static_folder='static', template_folder='templates')
 
 
-# ===== Cookies Policy =====
-@cookies_privacy_bp.route('/', methods=['GET', 'POST'])
+@cookies_privacy_bp.route('/', methods=['GET'])
 def cookies_privacy():
     return render_template('cookies-privacy.html', _theme='default')

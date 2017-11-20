ee5d01447321de636766c4071c98c8749d73dc9e
Andrew Millar <andrew.millar@qa.com>
2017-11-20 16:07:59 +0000

    Remove unused lines in cloudfoundry.py

--- frontstage/cloud/cloudfoundry.py
+++ frontstage/cloud/cloudfoundry.py
@@ -5,8 +5,6 @@ class ONSCloudFoundry(object):
 
     def __init__(self):
         self._cf_env = cfenv.AppEnv()
-        self._host = self._cf_env.uris[0].split(':') if self.detected else 'localhost'
-        self._protocol = 'https' if self.detected else 'http'
 
     @property
     def detected(self):

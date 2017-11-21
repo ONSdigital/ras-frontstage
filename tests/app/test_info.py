2a76ea519571c2ed058d9fc6a921e800ab84a417
andrewmil <Andrew.Millar@qa.com>
2017-11-21 13:42:30 +0000

    Add info endpoint test (#246)
    Added test for the info endpoint
    
    Also updated the run_tests.py script to use pytest

---
+++ tests/app/test_info.py
@@ -0,0 +1,16 @@
+import unittest
+
+from frontstage import app
+
+
+class TestInfo(unittest.TestCase):
+
+    def setUp(self):
+        self.app = app.test_client()
+
+    def test_info_with_git_info(self):
+        response = self.app.get("/info")
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('"name": "ras-frontstage"'.encode(), response.data)
+        self.assertIn('"test": "test"'.encode(), response.data)

577cdf75dfafdd8631c52a2cffcf22e0c062d34a
Adam Mann <manny160389@hotmail.com>
2018-04-05 12:27:16 +0100

    Remove get message route and some draft functinality

--- frontstage/views/secure_messaging/create_message.py
+++ frontstage/views/secure_messaging/create_message.py
@@ -28,11 +28,6 @@ def create_message(session):
         is_draft = form.save_draft.data
         sent_message = send_message(party_id, is_draft, case_id, survey, ru_ref)
 
-        # If draft was saved retrieve the saved draft
-        if is_draft:
-            logger.info('Draft sent successfully', message_id=sent_message['msg_id'], party_id=party_id)
-            return message_get('DRAFT', sent_message['msg_id'])
-
         return redirect(url_for('secure_message_bp.view_conversation_list'))
 
     else:
@@ -45,43 +40,6 @@ def create_message(session):
                                survey=survey, case_id=case_id,
                                form=form, errors=form.errors,
                                message=message.get('message', {}))
-
-
-@secure_message_bp.route('/<label>/<message_id>', methods=['GET'])
-@jwt_authorization(request)
-def message_get(session, label, message_id):
-    party_id = session['party_id']
-
-    message_json = get_message(message_id, label, party_id)
-    # Initialise SecureMessagingForm with values for the draft and hidden fields
-    draft = message_json['draft']
-    message = message_json['message']
-    form = SecureMessagingForm(formdata=None,
-                               thread_message_id=message.get('msg_id'),
-                               thread_id=message.get('thread_id'),
-                               msg_id=draft.get('msg_id'),
-                               hidden_subject=message.get('subject'),
-                               subject=draft.get('subject'),
-                               body=draft.get('body'))
-    # TODO this whole function needs looking at. When getting a draft it should use the draft end point not message
-    if label == "DRAFT":
-        ru_ref = draft.get('ru_id')
-        survey = draft.get('survey')
-        case_id = draft.get('case_id')
-    else:
-        ru_ref = message.get('ru_id')
-        survey = message.get('survey')
-        case_id = message.get('case_id')
-
-    return render_template('secure-messages/secure-messages-view.html',
-                           _theme='default',
-                           message=message,
-                           ru_ref=ru_ref,
-                           survey=survey,
-                           case_id=case_id,
-                           draft=draft,
-                           form=form,
-                           label=label)
 
 
 def send_message(party_id, is_draft, case_id, survey, ru_ref):

1aaee8ce4cb56110e8b32fbe2be5279a7191a2d4
Adam Mann <manny160389@hotmail.com>
2018-04-10 16:01:56 +0100

    Improve logging

--- frontstage/views/secure_messaging/message_get.py
+++ frontstage/views/secure_messaging/message_get.py
@@ -22,15 +22,16 @@ logger = wrap_logger(logging.getLogger(__name__))
 @secure_message_bp.route('/thread/<thread_id>', methods=['GET', 'POST'])
 @jwt_authorization(request)
 def view_conversation(session, thread_id):
-    logger.info("Getting conversation", thread_id=thread_id)
+    party_id = session.get('party_id')
+    logger.info("Getting conversation", thread_id=thread_id, party_id=party_id)
     # TODO, do we really want to do a GET every time, even if we're POSTing? Rops does it this
     # way so we can get it working, then get it right.
     conversation = get_conversation(thread_id)['messages']
-    logger.info("Successfully retrieved conversation", thread_id=thread_id)
+    logger.info("Successfully retrieved conversation", thread_id=thread_id, party_id=party_id)
     try:
         refined_conversation = [refine(message) for message in reversed(conversation)]
     except KeyError as e:
-        logger.exception("Message is missing important data", thread_id=thread_id)
+        logger.exception("Message is missing important data", thread_id=thread_id, party_id=party_id)
         raise
 
     if refined_conversation[-1]['unread']:
@@ -40,9 +41,9 @@ def view_conversation(session, thread_id):
     form.subject.data = refined_conversation[0].get('subject')
 
     if form.validate_on_submit():
-        logger.info("Sending message", thread_id=thread_id)
+        logger.info("Sending message", thread_id=thread_id, party_id=party_id)
         send_message(_get_message_json(form, refined_conversation[0], party_id=session['party_id']))
-        logger.info("Successfully sent message", thread_id=thread_id)
+        logger.info("Successfully sent message", thread_id=thread_id, party_id=party_id)
         thread_url = url_for("secure_message_bp.view_conversation", thread_id=thread_id) + "#latest-message"
         flash(Markup('Message sent. <a href={}>View Message</a>'.format(thread_url)))
         return redirect(url_for('secure_message_bp.view_conversation_list'))
@@ -56,13 +57,15 @@ def view_conversation(session, thread_id):
 @secure_message_bp.route('/threads', methods=['GET'])
 @jwt_authorization(request)
 def view_conversation_list(session):
-    logger.info("Getting conversation list")
+    party_id = session.get('party_id')
+    logger.info("Getting conversation list", party_id=party_id)
     conversation = get_conversation_list()
     try:
         refined_conversation = [refine(message) for message in conversation]
     except KeyError as e:
-        logger.exception("A key error occurred")
+        logger.exception("A key error occurred", party_id=party_id)
         raise ApiError(e)
+    logger.info("Retrieving and refining conversation successful", party_id=party_id)
 
     return render_template('secure-messages/conversation-list.html',
                            _theme='default',

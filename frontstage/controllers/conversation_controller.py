5843d0f103519e1ece5302037a4c8894515d9167
dcdarrell9 <darrells_king@hotmail.com>
2018-04-11 08:36:40 +0100

    Added session for request retry

--- frontstage/controllers/conversation_controller.py
+++ frontstage/controllers/conversation_controller.py
@@ -3,7 +3,9 @@ import logging
 
 from flask import current_app, request
 import requests
+from requests.adapters import HTTPAdapter
 from structlog import wrap_logger
+from urllib3 import Retry
 
 from frontstage.common.session import SessionHandler
 from frontstage.exceptions.exceptions import ApiError, AuthorizationTokenMissing, NoMessagesError
@@ -13,6 +15,13 @@ from requests.exceptions import HTTPError, RequestException
 logger = wrap_logger(logging.getLogger(__name__))
 
 
+# Configure number of retries on requests
+session = requests.Session()
+retries = Retry(total=10, backoff_factor=0.1)
+session.mount('http://', HTTPAdapter(max_retries=retries))
+session.mount('https://', HTTPAdapter(max_retries=retries))
+
+
 def get_conversation(thread_id):
     logger.info("Retrieving conversation", thread_id=thread_id)
 
@@ -20,7 +29,7 @@ def get_conversation(thread_id):
     url = '{}v2/threads/{}'.format(current_app.config["RAS_SECURE_MESSAGING_SERVICE"], thread_id)
 
     try:
-        response = requests.get(url, headers=headers)
+        response = session.get(url, headers=headers)
         response.raise_for_status()
     except (HTTPError, RequestException):
         logger.exception("Thread retrieval failed", thread_id=thread_id)
@@ -40,9 +49,8 @@ def get_conversation_list():
     headers = _create_get_conversation_headers()
     url = '{}threads'.format(current_app.config["RAS_SECURE_MESSAGING_SERVICE"])
 
-    response = requests.get(url, headers=headers)
-
     try:
+        response = session.get(url, headers=headers)
         response.raise_for_status()
     except HTTPError:
         logger.exception("Threads retrieval failed")
@@ -62,7 +70,7 @@ def send_message(message_json):
     url = '{}v2/messages'.format(current_app.config["RAS_SECURE_MESSAGING_SERVICE"])
     headers = _create_send_message_headers()
     try:
-        response = requests.post(url, headers=headers, data=message_json)
+        response = session.post(url, headers=headers, data=message_json)
         response.raise_for_status()
     except HTTPError as ex:
         logger.exception("Message sending failed due to API Error")

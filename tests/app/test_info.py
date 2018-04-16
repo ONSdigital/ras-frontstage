301ca6c547d182e06c6f63d039123f72f4cc1101
Ben Jefferies <benjefferies@echosoft.uk>
2018-04-16 10:53:41 +0100

    Apply security headers
    We need to add the following headers to response-operations-ui and
    frontstage
    
    ` Strict-Transport-Security: max-age=31536000; includeSubDomains` -
    Enforce all communication over TLS
    
    `Content-Security-Policy: default-src 'self'` - Mitigate and report XSS
    attacks. Do we need to allow google analytics?
    
    `X-XSS-Protection: 1` - Enables XSS filtering (usually default in
    browsers). If a cross-site scripting attack is detected, the browser
    will sanitize the page (remove the unsafe parts).
    
    `X-Content-Type-Options: nosniff` - Prevent mime sniffing
    
    `Referrer-Policy: same-origin` - Only send referrer information to the
    same domain

--- tests/app/test_info.py
+++ tests/app/test_info.py
@@ -14,3 +14,43 @@ class TestInfo(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertIn('"name": "ras-frontstage"'.encode(), response.data)
         self.assertIn('"test": "test"'.encode(), response.data)
+
+    def test_strict_transport_security_header_is_set(self):
+        with open('git_info', 'w') as outfile:
+            outfile.write('"test": "test"')
+
+        response = self.app.get("/info")
+
+        self.assertEqual(response.headers['Strict-Transport-Security'], 'max-age=31536000; includeSubDomains')
+
+    def test_content_security_policy_header_is_set(self):
+        with open('git_info', 'w') as outfile:
+            outfile.write('"test": "test"')
+
+        response = self.app.get("/info")
+
+        self.assertEqual(response.headers['Content-Security-Policy'], "default-src 'self'")
+
+    def test_x_xss_protection_header_is_set(self):
+        with open('git_info', 'w') as outfile:
+            outfile.write('"test": "test"')
+
+        response = self.app.get("/info")
+
+        self.assertEqual(response.headers['X-XSS-Protection'], '1')
+
+    def test_x_content_type_options_header_is_set(self):
+        with open('git_info', 'w') as outfile:
+            outfile.write('"test": "test"')
+
+        response = self.app.get("/info")
+
+        self.assertEqual(response.headers['X-Content-Type-Options'], 'nosniff')
+
+    def test_referrer_policy_header_is_set(self):
+        with open('git_info', 'w') as outfile:
+            outfile.write('"test": "test"')
+
+        response = self.app.get("/info")
+
+        self.assertEqual(response.headers['Referrer-Policy'], 'same-origin')

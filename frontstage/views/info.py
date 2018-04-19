a5300c03746ff008c8a2060a884ac30a0b2f98d2
Adam Mann <manny160389@hotmail.com>
2018-04-19 10:38:13 +0100

    Simplify info endpoint
    The git_info file didn't provide any value.  This makes the info endpoint return more sensible values.

--- frontstage/views/info.py
+++ frontstage/views/info.py
@@ -9,13 +9,7 @@ from frontstage import app
 
 
 logger = wrap_logger(logging.getLogger(__name__))
-
 info_bp = Blueprint('info_bp', __name__, static_folder='static', template_folder='templates')
-
-_health_check = {}
-if Path('git_info').exists():
-    with open('git_info') as io:
-        _health_check = json.loads(io.read())
 
 
 @info_bp.route('/', methods=['GET'])
@@ -23,7 +17,7 @@ def get_info():
     info = {
         "name": 'ras-frontstage',
         "version": app.config['VERSION'],
+        'status': 'OK',
     }
-    info = dict(_health_check, **info)
 
-    return make_response(jsonify(info), 200)
+    return jsonify(info)
--- tests/app/test_info.py
+++ tests/app/test_info.py
@@ -8,9 +8,9 @@ class TestInfo(unittest.TestCase):
     def setUp(self):
         self.app = app.test_client()
 
-    def test_info_with_git_info(self):
+    def test_info_endpoint(self):
         response = self.app.get("/info")
 
         self.assertEqual(response.status_code, 200)
         self.assertIn('"name": "ras-frontstage"'.encode(), response.data)
-        self.assertIn('"test": "test"'.encode(), response.data)
+        self.assertIn('"status": "OK"'.encode(), response.data)

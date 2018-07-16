5681c38e599291d828d60b77c1e6605e51398b7a
Mohammad Islam <MIslam92@users.noreply.github.com>
2018-07-16 08:35:51 +0100

    Update confirm organisation page to display ru trading name if present (#364)

--- frontstage/views/surveys/add_survey_confirm_organisation_survey.py
+++ frontstage/views/surveys/add_survey_confirm_organisation_survey.py
@@ -30,8 +30,8 @@ def survey_confirm_organisation(session):
         # Get organisation name
         case = case_controller.get_case_by_enrolment_code(enrolment_code)
         business_party_id = case['caseGroup']['partyId']
-        organisation_name = party_controller.get_party_by_business_id(business_party_id).get('name')
-
+        business_party = party_controller.get_party_by_business_id(business_party_id)
+        
         # Get survey name
         collection_exercise_id = case['caseGroup']['collectionExerciseId']
         collection_exercise = collection_exercise_controller.get_collection_exercise(collection_exercise_id)
@@ -41,12 +41,21 @@ def survey_confirm_organisation(session):
         logger.error('Failed to retrieve data for confirm add organisation/survey page', status_code=exc.status_code)
         raise
 
+    business_context = {
+        'enrolment_code': enrolment_code,
+        'case': case,
+        'trading_as': business_party.get('trading_as'),
+        'organisation': business_party.get('name'),
+        'collection_exercise_id': collection_exercise_id,
+        'survey_id': collection_exercise,
+        'survey_name': survey_name,
+    }
+
     logger.info('Successfully retrieved data for confirm add organisation/survey page',
                 collection_exercise_id=collection_exercise_id,
                 business_party_id=business_party_id,
                 survey_id=survey_id)
+
     return render_template('surveys/surveys-confirm-organisation.html',
-                           enrolment_code=enrolment_code,
-                           encrypted_enrolment_code=encrypted_enrolment_code,
-                           organisation_name=organisation_name,
-                           survey_name=survey_name)
+                           context=business_context,
+                           )

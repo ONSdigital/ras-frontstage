60dd348a673586c3bc302fcfdb615921d68d6683
Matt Simon <info@boxadesign.co.uk>
2018-08-23 14:29:04 +0100

    Updated to use v1.5.3 of the pattern library and code clean up

--- frontstage/assets/js/app/app.errors.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-import { setErrorEmitter } from './modules/errors.dom';
-import { errorEmitter as passwordValidationErrorEmitter } from './modules/password-validation.dom';
-
-setErrorEmitter(passwordValidationErrorEmitter);

5547f82bb108861080fcf6c416dc4cdba6a9da47
Benn Pearson <bennpearson@gmail.com>
2018-08-30 11:47:31 +0100

    Frontstage to use Pattern Library CDN (#359)

--- frontstage/assets/js/app/modules/password-validation.dom.e2e.js
+++
@@ -1,8 +0,0 @@
-describe('password validation', () => {
-
-	it('should do some assertions', function() {
-		browser.url('http://localhost:5001/reset-password');
-		expect(browser.getTitle()).toBe('Reset password');
-	});
-
-});

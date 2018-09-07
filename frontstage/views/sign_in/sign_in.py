28a5b25b5292a1875598978e7ed53ae6c4066728
Noor Uddin Ahmed <noor.ahmed@qa.com>
2018-09-07 13:23:42 +0100

    Updated party service with the ability to account lock (#395)

--- frontstage/views/sign_in/sign_in.py
+++ frontstage/views/sign_in/sign_in.py
@@ -7,6 +7,7 @@ from structlog import wrap_logger
 from frontstage import app
 from frontstage.common.session import SessionHandler
 from frontstage.controllers import oauth_controller, party_controller
+from frontstage.controllers.party_controller import change_respondent_status
 from frontstage.exceptions.exceptions import OAuth2Error
 from frontstage.jwt import encode, timestamp_token
 from frontstage.models import LoginForm
@@ -18,6 +19,7 @@ logger = wrap_logger(logging.getLogger(__name__))
 
 BAD_AUTH_ERROR = 'Unauthorized user credentials'
 NOT_VERIFIED_ERROR = 'User account not verified'
+USER_ACCOUNT_LOCKED = 'User account locked'
 
 
 @app.route('/', methods=['GET'])
@@ -46,6 +48,11 @@ def login():
         except OAuth2Error as exc:
             error_message = exc.oauth2_error
             if BAD_AUTH_ERROR in error_message:
+                return render_template('sign-in/sign-in.html', form=form, data={"error": {"type": "failed"}})
+            elif USER_ACCOUNT_LOCKED in error_message:
+                logger.info('User account is locked on the OAuth2 server')
+                if party_json['status'] == 'ACTIVE' or party_json['status'] == 'CREATED':
+                    change_respondent_status(respondent_id=party_id, status='SUSPENDED')
                 return render_template('sign-in/sign-in.html', form=form, data={"error": {"type": "failed"}})
             elif NOT_VERIFIED_ERROR in error_message:
                 logger.info('User account is not verified on the OAuth2 server')

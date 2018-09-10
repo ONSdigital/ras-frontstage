94ef3a047c13e61f3246da48280f3a2a9ee94fe2
Ryan Grundy <ryan.grundy@ons.gov.uk>
2018-09-10 10:12:50 +0100

    Call to notify

---
+++ frontstage/notifications/notifications.py
@@ -0,0 +1,36 @@
+import logging
+from urllib import parse as urlparse
+from flask import current_app
+import requests
+from requests import HTTPError
+from structlog import wrap_logger
+
+logger = wrap_logger(logging.getLogger(__name__))
+
+
+class AlertViaGovNotify:
+    """Notify Api handler"""
+
+    @staticmethod
+    def send(email, name):
+        notification = {
+            "emailAddress": email,
+            "name": name
+        }
+        if bool(email.strip()):
+            url = urlparse.urljoin(current_app.config['RM_NOTIFY_GATEWAY_URL'],
+                                   current_app.config['NOTIFICATION_TEMPLATE_ID'])
+
+            response = requests.post(url, auth=current_app.config['BASIC_AUTH'],
+                                     timeout=current_app.config['REQUESTS_POST_TIMEOUT'],
+                                     json=notification)
+            try:
+                response.raise_for_status()  # will raise HTTPError if not 2XX
+            except HTTPError:
+                logger.error(
+                    'This email {} , has not been sent, the service might be down or not available.')
+            else:
+                logger.info('Sent email notification, via RM Notify-Gateway to GOV.UK Notify.')
+
+        else:
+            logger.error('email has not been sent, email value is not present.')
--- frontstage/views/sign_in/sign_in.py
+++ frontstage/views/sign_in/sign_in.py
@@ -11,6 +11,7 @@ from frontstage.controllers.party_controller import change_respondent_status
 from frontstage.exceptions.exceptions import OAuth2Error
 from frontstage.jwt import encode, timestamp_token
 from frontstage.models import LoginForm
+from frontstage.notifications.notifications import AlertViaGovNotify
 from frontstage.views.sign_in import sign_in_bp
 
 
@@ -53,6 +54,7 @@ def login():
                 logger.info('User account is locked on the OAuth2 server')
                 if party_json['status'] == 'ACTIVE' or party_json['status'] == 'CREATED':
                     change_respondent_status(respondent_id=party_id, status='SUSPENDED')
+                AlertViaGovNotify().send(username, party_json['firstName'])
                 return render_template('sign-in/sign-in.account-locked.html', form=form)
             elif NOT_VERIFIED_ERROR in error_message:
                 logger.info('User account is not verified on the OAuth2 server')

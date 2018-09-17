3a78fbc5e18ee868cfa70a38df2acd8651542576
Nick Grant <nick.grant@ext.ons.gov.uk>
2018-09-17 11:43:48 +0100

    Get only the live collection excercises for survey

--- tests/app/controllers/test_collection_exercise_controller.py
+++ tests/app/controllers/test_collection_exercise_controller.py
@@ -1,5 +1,4 @@
 import unittest
-from unittest.mock import patch
 
 import responses
 from iso8601 import ParseError
@@ -8,7 +7,7 @@ from config import TestingConfig
 from frontstage import app
 from frontstage.controllers import collection_exercise_controller
 from frontstage.exceptions.exceptions import ApiError
-from tests.app.mocked_services import collection_exercise, collection_exercise_by_survey, survey, \
+from tests.app.mocked_services import collection_exercise, collection_exercise_by_survey, \
     url_get_collection_exercises_by_survey
 
 
@@ -34,20 +33,6 @@ class TestCollectionExerciseController(unittest.TestCase):
             with app.app_context():
                 with self.assertRaises(ApiError):
                     collection_exercise_controller.get_collection_exercises_for_survey(collection_exercise['surveyId'])
-
-    @patch('frontstage.controllers.collection_exercise_controller.get_collection_exercises_for_survey')
-    def test_get_live_collection_exercises_for_survey(self, get_collection_exercises_for_survey):
-        for ce in collection_exercise_by_survey:
-            if ce['events']:
-                ce['events'] = collection_exercise_controller.convert_events_to_new_format(ce['events'])
-
-        get_collection_exercises_for_survey.return_value = collection_exercise_by_survey
-
-        live_collection_exercises = collection_exercise_controller.get_live_collection_exercises_for_survey(survey['id'])
-
-        for live_collection_exercise in live_collection_exercises:
-            self.assertEqual(live_collection_exercise['state'], 'LIVE')
-            self.assertFalse(live_collection_exercise['events']['go_live']['is_in_future'])
 
     def test_convert_events_to_new_format_successful(self):
         formatted_events = collection_exercise_controller.convert_events_to_new_format(collection_exercise['events'])

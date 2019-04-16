74aefbd71312a0f7de146a781bcd0b074d1cce47
Owen C. Jones <owencjones@gmail.com>
2019-04-16 16:53:15 +0100

    About half of unit tests done

--- frontstage/i18n/translations.py
+++ frontstage/i18n/translations.py
@@ -6,32 +6,32 @@ from os.path import join
 
 logger = wrap_logger(logging.getLogger(__name__))
 
-translation_default_path = join(app.root_path, 'i18n')
-translation_path = app.config.get('TRANSLATIONS_PATH', translation_default_path)
-default_locale = 'en_GB'
-locale = app.config.get('DEFAULT_LOCALE', default_locale)
-
 
 class Translate:
 
     def __init__(self, translation_file):
+        translation_default_path = join(app.root_path, 'i18n')
+        self.translation_path = app.config.get('TRANSLATIONS_PATH', translation_default_path)
+        default_locale = 'en_GB'
+        self.locale = app.config.get('DEFAULT_LOCALE', default_locale)
+
         try:
             if not translation_file.endswith('.json'):
                 translation_file += '.json'
 
-            file_name = join(translation_path, translation_file)
+            file_name = join(self.translation_path, translation_file)
             with open(file_name) as f:
                 self.translations = load(f)
         except FileNotFoundError as exc:
             logger.error(f'Instantiating translation with file {file_name}')
-            raise(exc)
+            raise exc
         except Exception as exc:
             logger.error(f'An error occurred loading JSON from file {file_name}')
-            raise(exc)
+            raise exc
 
-    def translate(self, msgid, locale=default_locale):
+    def translate(self, msgid, locale):
 
-        active_locale = locale if locale in self.translations else default_locale
+        active_locale = locale if locale in self.translations else self.default_locale
 
         if active_locale not in self.translations:
             logger.debug(f'Defaulted to default locale for {msgid} because locale {locale} had message with that Id')
--- tests/app/test_translations.py
+++ tests/app/test_translations.py
@@ -1,36 +1,81 @@
+from os.path import join
 from unittest import TestCase
 from unittest.mock import patch, mock_open, MagicMock
 
 from frontstage.i18n.translations import Translate
+from frontstage import app
 
 
 class TestTranslations(TestCase):
 
     def setUp(self):
-        self.mock_open = mock_open()
+        self.app = app.test_client()
+        self.app.testing = True
 
     # Constructor tests
     def test_appends_json_to_filename_if_not_included(self):
-        with patch('builtins.open', new_callable=mock_open()) as m:
-            with patch('frontstage.i18n.translations.load', MagicMock()) as loadd:
-                instance = Translate('filename')
-                self.assertEqual(True, instance)
+        with patch('builtins.open', new_callable=mock_open()) as m_open:
+            with patch('frontstage.i18n.translations.load', MagicMock()) as m_load:
+                m_load.return_value = ''
+
+                Translate('filename')
+                self.assertEqual(m_open.call_count, 1)
+                self.assertTrue(m_open.call_args[0][0].endswith('filename.json'))
 
     def test_does_not_append_json_to_filename_if_included(self):
-        pass
+        with patch('builtins.open', new_callable=mock_open()) as m_open:
+            with patch('frontstage.i18n.translations.load', MagicMock()) as m_load:
+                m_load.return_value = ''
+
+                Translate('filename.json')
+                self.assertEqual(m_open.call_count, 1)
+                self.assertTrue(m_open.call_args[0][0].endswith('filename.json'))
 
     def test_uses_default_path_if_not_specified(self):
-        pass
+        with patch('builtins.open', new_callable=mock_open()) as m_open:
+            with patch('frontstage.i18n.translations.load', MagicMock()) as m_load:
+                m_load.return_value = ''
+
+                leading_path = join(app.root_path, 'i18n')
+
+                Translate('filename')
+                self.assertEqual(m_open.call_count, 1)
+                self.assertTrue(m_open.call_args[0][0].startswith(leading_path))
 
     def test_uses_config_path_if_specified(self):
-        pass
+        with patch('builtins.open', new_callable=mock_open()) as m_open:
+            with patch('frontstage.i18n.translations.load', MagicMock()) as m_load:
+                m_load.return_value = ''
+
+                app.config['TRANSLATIONS_PATH'] = 'PATH'
+                Translate('filename')
+
+                self.assertEqual(m_open.call_count, 1)
+                self.assertTrue(m_open.call_args[0][0].startswith('PATH'))
 
     def test_loads_json_and_assigns_to_instance(self):
-        pass
+        with patch('builtins.open', new_callable=mock_open()) as m_open:
+            with patch('frontstage.i18n.translations.load', MagicMock()) as m_load:
+                m_load.return_value = 'TEST VALUE'
 
-    def test_logs_and_raises_not_found_exception_if_file_missing(self):
-        pass
+                instance = Translate('filename.json')
+                self.assertEqual(instance.translations, 'TEST VALUE')
 
-    def test_logs_and_raises_other_exception_if_other_exception_occurs(self):
-        pass
+    def test_raises_not_found_exception_if_file_missing(self):
+        with patch('builtins.open', new_callable=mock_open()) as m_open:
+            with patch('frontstage.i18n.translations.load', MagicMock()) as m_load:
+                m_load.return_value = ''
+                m_open.side_effect = FileNotFoundError
+
+                with self.assertRaises(FileNotFoundError):
+                    Translate('filename.json')
+
+    def test_raises_other_exception_if_other_exception_occurs(self):
+        with patch('builtins.open', new_callable=mock_open()) as m_open:
+            with patch('frontstage.i18n.translations.load', MagicMock()) as m_load:
+                m_load.return_value = ''
+                m_open.side_effect = Exception
+
+                with self.assertRaises(Exception):
+                    Translate('filename.json')
 

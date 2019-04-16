13d7e221ae0050fb39007a5dcf17ee8aad2e0c07
Owen C. Jones <owencjones@gmail.com>
2019-04-16 11:58:59 +0100

    Added mocks

--- tests/app/test_translations.py
+++ tests/app/test_translations.py
@@ -1,24 +1,20 @@
 from unittest import TestCase
-from unittest.mock import patch, mock_open
+from unittest.mock import patch, mock_open, MagicMock
 
 from frontstage.i18n.translations import Translate
-
-from os.path import join
-from json import load
 
 
 class TestTranslations(TestCase):
 
     def setUp(self):
-        self.mock_load = patch('frontstage.i18n.translations.load')
+        self.mock_open = mock_open()
 
     # Constructor tests
     def test_appends_json_to_filename_if_not_included(self):
-        self.mock_load.return_value = ''
-        m = mock_open(read_data='')
-        with patch('frontstage.i18n.translations.open', m, create=True):
-            instance = Translate('filename')
-            self.assertEqual(True, instance)
+        with patch('builtins.open', new_callable=mock_open()) as m:
+            with patch('frontstage.i18n.translations.load', MagicMock()) as loadd:
+                instance = Translate('filename')
+                self.assertEqual(True, instance)
 
     def test_does_not_append_json_to_filename_if_included(self):
         pass

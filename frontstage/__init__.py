0f93cd5c6d89d63724b809409561ff235128e810
Owen C. Jones <owencjones@gmail.com>
2019-08-21 11:47:03 +0100

    Fix for tests broken by availability message

--- frontstage/__init__.py
+++ frontstage/__init__.py
@@ -12,8 +12,10 @@ csrf = CSRFProtect(app)
 
 @app.context_processor
 def inject_availability_message():
-    msg = str(redis.get('AVAILABILITY_MESSAGE').decode('utf-8'))
-    return dict(availability_message=msg)
+    msg = redis.get('AVAILABILITY_MESSAGE')
+    if msg:
+        return dict(availability_message=msg)
+    return dict()
 
 # Bind routes to app
 import frontstage.views  # NOQA  # pylint: disable=wrong-import-position

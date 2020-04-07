56b09268a4aa9aa37a83e99fb6e1cd726a3f9e93
Warren Bailey <warren@warrenbailey.net>
2020-04-07 16:31:40 +0100

    Fix linting

--- frontstage/__init__.py
+++ frontstage/__init__.py
@@ -6,6 +6,7 @@ redis = redis.StrictRedis(host=app.config['REDIS_HOST'],
                           port=app.config['REDIS_PORT'],
                           db=app.config['REDIS_DB'])
 
+
 @app.context_processor
 def inject_availability_message():
     if len(redis.keys('AVAILABILITY_MESSAGE')) == 1:

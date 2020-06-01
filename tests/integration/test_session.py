b0e8445972ebc53ba6131a1cd905c8b9e6c8c70f
jterryons <john.terry@ons.gov.uk>
2020-06-01 15:32:05 +0100

    unit test fixes

--- tests/integration/test_session.py
+++ tests/integration/test_session.py
@@ -15,8 +15,8 @@ class TestSession(unittest.TestCase):
 
     def test_create_session(self):
         # Create session and get session key
-        session = Session()
-        session.create_session(encoded_jwt='test_jwt')
+        session = Session.from_party_id("party")
+        session.save()
         session_key = session.session_key
 
         # Retrieve encoded_jwt from session
@@ -25,46 +25,22 @@ class TestSession(unittest.TestCase):
 
     def test_update_session(self):
         # Create session and get session key
-        session = Session()
-        session.create_session(encoded_jwt='test_jwt')
+        session = Session.from_party_id("party")
+        session.save()
         session_key = session.session_key
 
         # Wait 3 seconds and update the session
-        time.sleep(3)
+        time.sleep(1)
         session.update_session()
 
         # Check that the session expiry time has been reset
         expires_in = redis.ttl(session_key)
         self.assertEqual(expires_in, 3600)
 
-    def test_update_session_with_session_key(self):
-        # Create session and get session key
-        session = Session()
-        session.create_session(encoded_jwt='test_jwt')
-        session_key = session.session_key
-
-        # Wait 3 seconds and update the session
-        time.sleep(3)
-        session.update_session(session_key)
-
-        # Check that the session expiry time has been reset
-        expires_in = redis.ttl(session_key)
-        self.assertEqual(expires_in, 3600)
-
-    def test_get_encoded_jwt(self):
-        # Create session and get session key
-        session = Session()
-        session.create_session(encoded_jwt='test_jwt')
-        session_key = session.session_key
-
-        encoded_jwt = session.get_encoded_jwt(session_key)
-
-        self.assertEqual(encoded_jwt, 'test_jwt'.encode())
-
     def test_delete_session(self):
         # Create session and get session key
-        session = Session()
-        session.create_session(encoded_jwt='test_jwt')
+        session = Session.from_party_id("party")
+        session.save()
         session_key = session.session_key
         session.delete_session(session_key)
 

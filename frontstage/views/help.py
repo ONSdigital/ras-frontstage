d89404bf525e08efe971bc4afaa57dfcbad7aae9
TomHolroyd <tomholroyd96@gmail.com>
2020-09-29 14:59:53 +0100

    Vulnerability disclosure policy page Frontstage
    Signed-off-by: TomHolroyd <tomholroyd96@gmail.com>

---
+++ frontstage/views/help.py
@@ -0,0 +1,15 @@
+import logging
+
+from flask import Blueprint, render_template
+from structlog import wrap_logger
+
+
+logger = wrap_logger(logging.getLogger(__name__))
+
+help_bp = Blueprint('help_bp', __name__,
+                          static_folder='static', template_folder='templates')
+
+
+@help_bp.route('/disclosure', methods=['GET'])
+def help():
+    return render_template('vunerability_disclosure.html')

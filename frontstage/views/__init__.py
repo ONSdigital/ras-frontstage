5226f1098489e95b18574b48203fd13376ea0719
Thomas Holroyd <47748295+TomHolroyd@users.noreply.github.com>
2020-10-16 11:05:52 +0100

    Vulnerability disclosure policy page Frontstage (#623)
    * Vulnerability disclosure policy page Frontstage
    
    Signed-off-by: TomHolroyd <tomholroyd96@gmail.com>
    
    * Config changed back
    
    * [ci skip] auto patch increment
    skip-checks: true
    
    * rename a few things
    
    * add disclosure page
    
    * break up lines
    
    * [ci skip] auto patch increment
    skip-checks: true
    
    Co-authored-by: ras-rm-bot <65395820+ras-rm-bot@users.noreply.github.com>
    Co-authored-by: jterryons <john.terry@ons.gov.uk>

--- frontstage/views/__init__.py
+++ frontstage/views/__init__.py
@@ -8,6 +8,9 @@ from frontstage.views.register import register_bp
 from frontstage.views.secure_messaging import secure_message_bp
 from frontstage.views.sign_in import sign_in_bp
 from frontstage.views.surveys import surveys_bp
+from frontstage.views.security import security_bp
+from frontstage.views.help import help_bp
+
 
 
 # Import endpoints and register blueprints
@@ -20,3 +23,5 @@ app.register_blueprint(secure_message_bp, url_prefix='/secure-message')
 app.register_blueprint(sign_in_bp, url_prefix='/sign-in')
 app.register_blueprint(surveys_bp, url_prefix='/surveys')
 app.register_blueprint(register_bp, url_prefix='/register')
+app.register_blueprint(security_bp, url_prefix='/.well-known')
+app.register_blueprint(help_bp, url_prefix='/help')

b1060f673d0553211ff96538752247c41d1b37ba
jdfox <jfoxgithub@gmail.com>
2020-11-25 10:39:10 +0000

    rv set_cookie change

--- frontstage/views/sign_in/sign_in.py
+++ frontstage/views/sign_in/sign_in.py
@@ -91,8 +91,8 @@ def login():  # noqa: C901
         response.set_cookie('authorization',
                             value=session.session_key,
                             expires=session.get_expires_in(),
-                            secure=True,
-                            httponly=True)
+                            secure=secure,
+                            httponly=secure)
         count = conversation_controller.get_message_count_from_api(session)
         session.set_unread_message_total(count)
         bound_logger.info('Successfully created session', session_key=session.session_key)

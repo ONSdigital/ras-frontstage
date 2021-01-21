6e3efbed23bdd4d7c439dcc2154fe0c6a8085996
Amit Sinha <aks.sinha.amit@icloud.com>
2021-01-21 14:58:20 +0000

    Change my contact details changes

--- frontstage/views/account/account.py
+++ frontstage/views/account/account.py
@@ -1,20 +1,76 @@
 import logging
 
-from flask import render_template, request
+from flask import render_template, request, flash, url_for
 from structlog import wrap_logger
+from werkzeug.utils import redirect
 
 from frontstage.common.authorisation import jwt_authorization
 from frontstage.controllers import party_controller
+from frontstage.exceptions.exceptions import ApiError
+from frontstage.models import OptionsForm, ContactDetailsChangeForm
 
 from frontstage.views.account import account_bp
 
 logger = wrap_logger(logging.getLogger(__name__))
 
 
-@account_bp.route('/', methods=['GET'])
+@account_bp.route('/', methods=['GET', 'POST'])
 @jwt_authorization(request)
 def get_account(session):
+    form = OptionsForm()
+    form_valid = form.validate()
     party_id = session.get_party_id()
     respondent_details = party_controller.get_respondent_party_by_id(party_id)
-    return render_template('account/account.html', respondent=respondent_details)
+    if request.method == 'POST':
+        if not form_valid:
+            flash('At least one option should be selected!')
+            return redirect(url_for('account_bp.get_account'))
+        if form.data['option'] == 'contact_details':
+            return redirect(url_for('account_bp.change_account_details'))
+    else:
+        return render_template('account/account.html', form=form, respondent=respondent_details)
 
+
+@account_bp.route('/change-account-details', methods=['GET', 'POST'])
+@jwt_authorization(request)
+def change_account_details(session):
+    form = ContactDetailsChangeForm(request.values)
+    party_id = session.get_party_id()
+    respondent_details = party_controller.get_respondent_party_by_id(party_id)
+    update_required_flag = False
+    if request.method == 'POST' and form.validate():
+        logger.info('Attempting to create account')
+        message = "We've updated your"
+        if request.form.get('first_name') != respondent_details['firstName']:
+            respondent_details['firstName'] = request.form.get('first_name')
+            update_required_flag = True
+            message += " first name"
+        if request.form.get('last_name') != respondent_details['lastName']:
+            respondent_details['lastName'] = request.form.get('last_name')
+            update_required_flag = True
+            if message == "We've updated your":
+                message += " last name"
+            else:
+                message += ", last name"
+        if request.form.get('phone_number') != respondent_details['telephone']:
+            respondent_details['telephone'] = request.form.get('phone_number')
+            update_required_flag = True
+            if message == "We've updated your":
+                message += " phone number"
+            else:
+                message += ", phone number"
+
+        if update_required_flag:
+            try:
+                party_controller.update_account(respondent_details)
+            except ApiError as exc:
+                logger.error('Failed to updated account', status=exc.status_code)
+                raise exc
+            logger.info('Successfully updated account')
+            success_panel = message
+            return redirect(url_for('surveys_bp.get_survey_list', tag='todo', success_panel=success_panel))
+        else:
+            return redirect(url_for('surveys_bp.get_survey_list', tag='todo'))
+    else:
+        return render_template('account/account.contact-detail-change.html',
+                               form=form, errors=form.errors, respondent=respondent_details)
--- tests/integration/views/account/test_account.py
+++ tests/integration/views/account/test_account.py
@@ -1,7 +1,7 @@
 import unittest
 from unittest.mock import patch
 
-from frontstage import app
+from frontstage import app, create_app_object
 from tests.integration.mocked_services import encoded_jwt_token, respondent_party
 
 
@@ -11,9 +11,13 @@ class TestSurveyList(unittest.TestCase):
         self.app = app.test_client()
         self.app.set_cookie('localhost', 'authorization', 'session_key')
         self.headers = {
-            "Authorization": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoicmluZ3JhbUBub3d3aGVyZS5jb20iLCJ1c2VyX3Njb3BlcyI6WyJjaS5yZWFkIiwiY2kud3JpdGUiXX0.se0BJtNksVtk14aqjp7SvnXzRbEKoqXb8Q5U9VVdy54"  # NOQA
-            }
+            "Authorization": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoicmluZ3JhbUBub3d3aGVyZS5jb20iLCJ1c2VyX3Njb3BlcyI6WyJjaS5yZWFkIiwiY2kud3JpdGUiXX0.se0BJtNksVtk14aqjp7SvnXzRbEKoqXb8Q5U9VVdy54"
+            # NOQA
+        }
         self.patcher = patch('redis.StrictRedis.get', return_value=encoded_jwt_token)
+        self.contact_details_form = {
+            "option": "contact_details"
+        }
         self.patcher.start()
 
     def tearDown(self):
@@ -28,3 +32,34 @@ class TestSurveyList(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTrue('example@example.com'.encode() in response.data)
         self.assertTrue('0987654321'.encode() in response.data)
+
+    @patch('frontstage.controllers.party_controller.get_respondent_party_by_id')
+    def test_account_options(self, get_respondent_party_by_id):
+        get_respondent_party_by_id.return_value = respondent_party
+
+        response = self.app.get('/my-account')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('example@example.com'.encode() in response.data)
+        self.assertTrue('0987654321'.encode() in response.data)
+        self.assertIn("Help with your account".encode(), response.data)
+
+    def test_account_options_selection_none(self):
+        response = self.app.post('/my-account', data={
+            "option": None
+        }, follow_redirects=True)
+        self.assertIn("At least one option should be selected!".encode(), response.data)
+
+    @patch('frontstage.controllers.party_controller.get_respondent_party_by_id')
+    def test_account_options_selection(self, get_respondent_party_by_id):
+        get_respondent_party_by_id.return_value = respondent_party
+        response = self.app.post('/my-account', data=self.contact_details_form, follow_redirects=True)
+        self.assertIn("For international numbers include the country code, for example +33 1234 567 890".encode(),
+                      response.data)
+
+    @patch('frontstage.controllers.party_controller.get_respondent_party_by_id')
+    def test_account_contact_details_error(self, get_respondent_party_by_id):
+        get_respondent_party_by_id.return_value = respondent_party
+        response = self.app.post('/my-account/change-account-details',
+                                 data={"first_name": ""}, follow_redirects=True)
+        self.assertIn("Problem with the first name".encode(), response.data)

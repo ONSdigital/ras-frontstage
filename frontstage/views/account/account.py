9b8992f6a942e1fdf84795cfbb9111131a66d826
Amit Sinha <aks.sinha.amit@icloud.com>
2021-01-25 15:04:19 +0000

    review comment fixes

--- frontstage/views/account/account.py
+++ frontstage/views/account/account.py
@@ -14,21 +14,26 @@ from frontstage.views.account import account_bp
 logger = wrap_logger(logging.getLogger(__name__))
 
 
-@account_bp.route('/', methods=['GET', 'POST'])
+@account_bp.route('/', methods=['GET'])
 @jwt_authorization(request)
 def get_account(session):
     form = OptionsForm()
-    form_valid = form.validate()
     party_id = session.get_party_id()
     respondent_details = party_controller.get_respondent_party_by_id(party_id)
-    if request.method == 'POST':
-        if not form_valid:
-            flash('At least one option should be selected!')
-            return redirect(url_for('account_bp.get_account'))
-        if form.data['option'] == 'contact_details':
-            return redirect(url_for('account_bp.change_account_details'))
-    else:
-        return render_template('account/account.html', form=form, respondent=respondent_details)
+    error_panel = request.args.get('error_panel')
+    return render_template('account/account.html', form=form, respondent=respondent_details, error_panel=error_panel)
+
+
+@account_bp.route('/', methods=['POST'])
+@jwt_authorization(request)
+def update_account(session):
+    form = OptionsForm()
+    form_valid = form.validate()
+    if not form_valid:
+        error_panel = 'At least one option should be selected.'
+        return redirect(url_for('account_bp.get_account', error_panel=error_panel))
+    if form.data['option'] == 'contact_details':
+        return redirect(url_for('account_bp.change_account_details'))
 
 
 @account_bp.route('/change-account-details', methods=['GET', 'POST'])
@@ -38,28 +43,10 @@ def change_account_details(session):
     party_id = session.get_party_id()
     respondent_details = party_controller.get_respondent_party_by_id(party_id)
     update_required_flag = False
+    attributes_changed = []
     if request.method == 'POST' and form.validate():
-        logger.info('Attempting to create account')
-        message = "We've updated your"
-        if request.form.get('first_name') != respondent_details['firstName']:
-            respondent_details['firstName'] = request.form.get('first_name')
-            update_required_flag = True
-            message += " first name"
-        if request.form.get('last_name') != respondent_details['lastName']:
-            respondent_details['lastName'] = request.form.get('last_name')
-            update_required_flag = True
-            if message == "We've updated your":
-                message += " last name"
-            else:
-                message += ", last name"
-        if request.form.get('phone_number') != respondent_details['telephone']:
-            respondent_details['telephone'] = request.form.get('phone_number')
-            update_required_flag = True
-            if message == "We've updated your":
-                message += " phone number"
-            else:
-                message += ", phone number"
-
+        logger.info('Attempting to update contact details changes on the account')
+        update_required_flag = check_attribute_change(attributes_changed, respondent_details, update_required_flag)
         if update_required_flag:
             try:
                 party_controller.update_account(respondent_details)
@@ -67,10 +54,38 @@ def change_account_details(session):
                 logger.error('Failed to updated account', status=exc.status_code)
                 raise exc
             logger.info('Successfully updated account')
-            success_panel = message
+            success_panel = create_success_message(attributes_changed, "We've updated your ")
             return redirect(url_for('surveys_bp.get_survey_list', tag='todo', success_panel=success_panel))
         else:
             return redirect(url_for('surveys_bp.get_survey_list', tag='todo'))
     else:
-        return render_template('account/account.contact-detail-change.html',
+        return render_template('account/account-contact-detail-change.html',
                                form=form, errors=form.errors, respondent=respondent_details)
+
+
+def check_attribute_change(attributes_changed, respondent_details, update_required_flag):
+    if request.form.get('first_name') != respondent_details['firstName']:
+        respondent_details['firstName'] = request.form.get('first_name')
+        update_required_flag = True
+        attributes_changed.append('first name')
+    if request.form.get('last_name') != respondent_details['lastName']:
+        respondent_details['lastName'] = request.form.get('last_name')
+        update_required_flag = True
+        attributes_changed.append('last name')
+    if request.form.get('phone_number') != respondent_details['telephone']:
+        respondent_details['telephone'] = request.form.get('phone_number')
+        update_required_flag = True
+        attributes_changed.append('telephone number')
+    return update_required_flag
+
+
+def create_success_message(attr, message):
+    for x in attr:
+        if x == attr[-1] and len(attr) >= 2:
+            message += ' and ' + x
+        elif x != attr[0] and len(attr) > 2:
+            message += ', ' + x
+        else:
+            message += x
+
+    return message
--- tests/integration/views/account/test_account.py
+++ tests/integration/views/account/test_account.py
@@ -1,8 +1,10 @@
 import unittest
 from unittest.mock import patch
 
-from frontstage import app, create_app_object
-from tests.integration.mocked_services import encoded_jwt_token, respondent_party
+import requests_mock
+
+from frontstage import app
+from tests.integration.mocked_services import encoded_jwt_token, respondent_party, survey_list_todo
 
 
 class TestSurveyList(unittest.TestCase):
@@ -45,6 +47,13 @@ class TestSurveyList(unittest.TestCase):
         self.assertIn("Help with your account".encode(), response.data)
 
     @patch('frontstage.controllers.party_controller.get_respondent_party_by_id')
+    def test_account_options_not_selection(self, get_respondent_party_by_id):
+        get_respondent_party_by_id.return_value = respondent_party
+        response = self.app.post('/my-account', data={"option": None}, follow_redirects=True)
+        self.assertIn("At least one option should be selected.".encode(),
+                      response.data)
+
+    @patch('frontstage.controllers.party_controller.get_respondent_party_by_id')
     def test_account_options_selection(self, get_respondent_party_by_id):
         get_respondent_party_by_id.return_value = respondent_party
         response = self.app.post('/my-account', data=self.contact_details_form, follow_redirects=True)
@@ -57,3 +66,18 @@ class TestSurveyList(unittest.TestCase):
         response = self.app.post('/my-account/change-account-details',
                                  data={"first_name": ""}, follow_redirects=True)
         self.assertIn("Problem with the first name".encode(), response.data)
+
+    @patch('frontstage.controllers.party_controller.get_respondent_party_by_id')
+    @patch('frontstage.controllers.party_controller.update_account')
+    @patch('frontstage.controllers.party_controller.get_survey_list_details_for_party')
+    def test_account_contact_details_success(self,
+                                             get_survey_list,
+                                             update_account,
+                                             get_respondent_party_by_id):
+        get_respondent_party_by_id.return_value = respondent_party
+        get_survey_list.return_value = survey_list_todo
+        response = self.app.post('/my-account/change-account-details',
+                                 data={"first_name": "test account",
+                                       "last_name": "test_account",
+                                       "phone_number": "07772257773"}, follow_redirects=True)
+        self.assertIn("We&#39;ve updated your first name, last name and telephone number".encode(), response.data)

73d170c73409a755adf17a675bfed7995466d0b2
jdfox <jfoxgithub@gmail.com>
2021-01-26 15:36:14 +0000

    fix auth

--- tests/unit/test_message_get.py
+++ tests/unit/test_message_get.py
@@ -1,17 +1,24 @@
 import copy
 import unittest
 import requests_mock
+from unittest.mock import patch
 
 from frontstage import app
 from frontstage.views.secure_messaging.message_get import get_msg_to
 from tests.integration.mocked_services import (message_json, url_get_conversation_count,
-                                               url_get_thread, url_get_survey_long_name)
+                                               url_get_thread, url_get_survey_long_name,
+                                               encoded_jwt_token)
 
 class TestMessageGet(unittest.TestCase):
 
     def setUp(self):
         self.app = app.test_client()
         self.app.set_cookie('localhost', 'authorization', 'session_key')
+        self.patcher = patch('redis.StrictRedis.get', return_value=encoded_jwt_token)
+        self.patcher.start()
+        self.headers = {
+            "Authorization": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoicmluZ3JhbUBub3d3aGVyZS5jb20iLCJ1c2VyX3Njb3BlcyI6WyJjaS5yZWFkIiwiY2kud3JpdGUiXX0.se0BJtNksVtk14aqjp7SvnXzRbEKoqXb8Q5U9VVdy54" # NOQA
+        }
 
     def test_get_msg_to_returns_group_if_no_internal_user(self):
         msg = {'something': 'somethings'}

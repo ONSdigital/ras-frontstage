c001b956e0892033923026e2d6f062c60a346609
Adam Mann <manny160389@hotmail.com>
2021-02-03 10:13:42 +0000

    Add docstring and tone down the logging

--- frontstage/controllers/banner_controller.py
+++ frontstage/controllers/banner_controller.py
@@ -8,7 +8,12 @@ logger = wrap_logger(logging.getLogger(__name__))
 
 
 def current_banner():
-    logger.info('Attempting to retrieve the current live banner')
+    """
+    Calls the banner service to get the currently active banner.  This function logs very little by design as
+    it's going to be called a lot and we don't want to spam the logs.
+
+    :return: The banner text, if any, else an empty string
+    """
     url = f"{app.config['BANNER_SERVICE_URL']}/banner"
     response = requests.get(url)
     try:
@@ -18,9 +23,6 @@ def current_banner():
             logger.error('Failed to retrieve Banner from api')
         return ""
 
-    logger.info('Successfully retrieved current live banner from api')
-    if response.status_code == 204:
-        return ""
     banner = response.json()
     content = banner.get('content', "")
     return content

98956126d49dfd6aa1dd0abbc1e62284e731d2f3
Adam Mann <manny160389@hotmail.com>
2021-02-05 11:14:55 +0000

    Update old test to reflect reality

--- tests/integration/test_availability_message.py
+++ tests/integration/test_availability_message.py
@@ -5,7 +5,7 @@ from frontstage import app
 import requests_mock
 from tests.integration.mocked_services import url_banner_api
 
-TESTMSG = b'THIS IS A TEST MESSAGE'
+TESTMSG = 'THIS IS A TEST MESSAGE'
 
 
 class TestAvailabilityMessage(TestCase):
@@ -15,18 +15,18 @@ class TestAvailabilityMessage(TestCase):
         self.app = app.test_client()
 
     @requests_mock.mock()
-    def test_message_does_not_show_if_redis_flag_not_set(self, mock_request):
+    def test_message_does_not_show_no_banner_set(self, mock_request):
         mock_request.get(url_banner_api, status_code=404)
         response = self.app.get('/', follow_redirects=True)
 
         self.assertEqual(response.status_code, 200)
-        self.assertFalse(TESTMSG in response.data)
+        self.assertFalse(TESTMSG.encode() in response.data)
 
     @requests_mock.mock()
-    def test_message_shows_correct_message_if_redis_flag_set(self, mock_request):
-        mock_request.get(url_banner_api, status_code=200, text=f'{{"content":"{TESTMSG}"}}')
+    def test_message_shows_correct_message_banner_is_set(self, mock_request):
+        mock_request.get(url_banner_api, status_code=200, json={"id": "active", "content": TESTMSG})
 
         response = self.app.get('/', follow_redirects=True)
 
         self.assertEqual(response.status_code, 200)
-        self.assertTrue(TESTMSG in response.data)
+        self.assertTrue(TESTMSG.encode() in response.data)

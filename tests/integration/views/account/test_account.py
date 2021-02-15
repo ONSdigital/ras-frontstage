ef535ecca61722f8f76922f82809fb3b8df5bf0f
Amit Sinha <aks.sinha.amit@icloud.com>
2021-02-15 15:30:48 +0000

    test case failure update

--- tests/integration/views/account/test_account.py
+++ tests/integration/views/account/test_account.py
@@ -1,8 +1,10 @@
 import unittest
+import responses
 from unittest.mock import patch
 
 from frontstage import app
-from tests.integration.mocked_services import encoded_jwt_token, respondent_party, survey_list_todo
+from tests.integration.mocked_services import encoded_jwt_token, respondent_party, survey_list_todo, \
+    url_get_respondent_party
 
 
 class TestSurveyList(unittest.TestCase):
@@ -84,34 +86,40 @@ class TestSurveyList(unittest.TestCase):
         self.assertIn("updated your first name, last name and telephone number".encode(), response.data)
 
     @patch('frontstage.controllers.party_controller.get_respondent_party_by_id')
-    @patch('frontstage.controllers.party_controller.update_account')
     @patch('frontstage.controllers.party_controller.get_survey_list_details_for_party')
+    @patch('frontstage.controllers.party_controller.is_email_already_used_to_register')
     def test_account_change_account_email_address(self,
+                                                  is_email_already_used_to_register,
                                                   get_survey_list,
-                                                  update_account,
                                                   get_respondent_party_by_id):
         get_respondent_party_by_id.return_value = respondent_party
         get_survey_list.return_value = survey_list_todo
-        response = self.app.post('/my-account/change-account-details',
-                                 data={"first_name": "test account",
-                                       "last_name": "test_account",
-                                       "phone_number": "07772257773",
-                                       "email_address": "exampleone@example.com"}, follow_redirects=True)
-        self.assertIn("updated your first name, last name and telephone number".encode(), response.data)
-        self.assertIn("Change email address".encode(), response.data)
-        self.assertIn("You will need to authorise a change of email address.".encode(), response.data)
-        self.assertIn("We will send a confirmation email to".encode(), response.data)
-        self.assertIn("exampleone@example.com".encode(), response.data)
+        is_email_already_used_to_register.return_value = False
+        with responses.RequestsMock() as rsps:
+            rsps.add(rsps.PUT, url_get_respondent_party, json=respondent_party, status=200)
+            response = self.app.post('/my-account/change-account-details',
+                                     data={"first_name": "test account",
+                                           "last_name": "test_account",
+                                           "phone_number": "07772257773",
+                                           "email_address": "exampleone@example.com"}, follow_redirects=True)
+            self.assertIn("updated your first name, last name and telephone number".encode(), response.data)
+            self.assertIn("Change email address".encode(), response.data)
+            self.assertIn("You will need to authorise a change of email address.".encode(), response.data)
+            self.assertIn("We will send a confirmation email to".encode(), response.data)
+            self.assertIn("exampleone@example.com".encode(), response.data)
 
     @patch('frontstage.controllers.party_controller.get_respondent_party_by_id')
     @patch('frontstage.controllers.party_controller.update_account')
     @patch('frontstage.controllers.party_controller.get_survey_list_details_for_party')
+    @patch('frontstage.controllers.party_controller.is_email_already_used_to_register')
     def test_account_change_account_email_addres_almost_done(self,
+                                                             is_email_already_used_to_register,
                                                              get_survey_list,
                                                              update_account,
                                                              get_respondent_party_by_id):
         get_respondent_party_by_id.return_value = respondent_party
         get_survey_list.return_value = survey_list_todo
+        is_email_already_used_to_register.return_value = False
         response = self.app.post('/my-account/change-account-email-address',
                                  data={"email_address": "exampleone@example.com"}, follow_redirects=True)
         self.assertIn("Almost done".encode(), response.data)
@@ -129,8 +137,7 @@ class TestSurveyList(unittest.TestCase):
                                   get_respondent_party_by_id):
         get_respondent_party_by_id.return_value = respondent_party
         get_survey_list.return_value = survey_list_todo
-        is_value = True
-        is_email_already_used_to_register.return_value = is_value
+        is_email_already_used_to_register.return_value = True
         response = self.app.post('/my-account/change-account-details',
                                  data={"first_name": "new first name",
                                        "last_name": "new last name",

8f618f7d18176e7828e713a7eef5ee25307743f8
Amit Sinha <aks.sinha.amit@icloud.com>
2021-02-16 13:20:22 +0000

    update

--- tests/integration/views/account/test_account.py
+++ tests/integration/views/account/test_account.py
@@ -26,12 +26,13 @@ class TestSurveyList(unittest.TestCase):
     @patch('frontstage.controllers.party_controller.get_respondent_party_by_id')
     def test_account(self, get_respondent_party_by_id):
         get_respondent_party_by_id.return_value = respondent_party
+        with app.app_context():
+            app.config['ACCOUNT_CHANGE_FLAG'] = True
+            response = self.app.get('/my-account')
 
-        response = self.app.get('/my-account')
-
-        self.assertEqual(response.status_code, 200)
-        self.assertTrue('example@example.com'.encode() in response.data)
-        self.assertTrue('0987654321'.encode() in response.data)
+            self.assertEqual(response.status_code, 200)
+            self.assertTrue('example@example.com'.encode() in response.data)
+            self.assertTrue('0987654321'.encode() in response.data)
 
     @patch('frontstage.controllers.party_controller.get_respondent_party_by_id')
     def test_account_options(self, get_respondent_party_by_id):
@@ -43,7 +44,6 @@ class TestSurveyList(unittest.TestCase):
             self.assertEqual(response.status_code, 200)
             self.assertTrue('example@example.com'.encode() in response.data)
             self.assertIn("Help with your account".encode(), response.data)
-
 
     @patch('frontstage.controllers.party_controller.get_respondent_party_by_id')
     def test_account_options_not_selection(self, get_respondent_party_by_id):

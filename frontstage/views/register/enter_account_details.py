d68ceb7f47613475b36f660f1506d535fee75407
Amit Sinha <aks.sinha.amit@icloud.com>
2021-05-26 13:57:19 +0100

    Share surveys story 5 updates for frontstage

--- frontstage/views/register/enter_account_details.py
+++ frontstage/views/register/enter_account_details.py
@@ -1,12 +1,13 @@
 import logging
 
-from flask import render_template, request
+from flask import render_template, request, url_for
 from structlog import wrap_logger
+from werkzeug.utils import redirect
 
 from frontstage.common.cryptographer import Cryptographer
 from frontstage.controllers import iac_controller, party_controller
 from frontstage.exceptions.exceptions import ApiError
-from frontstage.models import RegistrationForm
+from frontstage.models import RegistrationForm, ShareSurveyRegistrationForm
 from frontstage.views.register import register_bp
 
 
@@ -53,3 +54,53 @@ def register_enter_your_details():
 
     else:
         return render_template('register/register.enter-your-details.html', form=form, errors=form.errors)
+
+
+@register_bp.route('/share-surveys/create-account/enter-account-details', methods=['GET', 'POST'])
+def share_surveys_register_enter_your_details():
+    """
+     Registration endpoint for account creation and verification against share surveys (Account does not exist)
+    :return:
+    :rtype:
+    """
+    # Get and decrypt enrolment code
+    batch_no = request.args.get('batch_no', None)
+    email = request.args.get('email', None)
+    form = ShareSurveyRegistrationForm(request.values, batch_no=batch_no, email=email)
+
+    # Validate batch_no before rendering or checking the form
+    party_controller.get_share_surveys_batch_number(batch_no)
+
+    if request.method == 'POST' and form.validate():
+        logger.info('Attempting to create account against share surveys email address')
+        email_address = form.email.data
+        registration_data = {
+            'emailAddress': email_address,
+            'firstName': request.form.get('first_name'),
+            'lastName': request.form.get('last_name'),
+            'password': request.form.get('password'),
+            'telephone': request.form.get('phone_number'),
+            'batch_no': batch_no,
+        }
+
+        try:
+            logger.info('Calling party service to create account against share surveys email address')
+            party_controller.create_share_survey_account(registration_data)
+        except ApiError as exc:
+            if exc.status_code == 400:
+                logger.info('share surveys email address already in use')
+                error = {"email_address": ["This email has already been used to register an account"]}
+                return render_template('register/register-share-survey-enter-your-details.html', form=form,
+                                       errors=error,
+                                       email=email)
+            else:
+                logger.error('Failed to create/register new account  against share surveys email address',
+                             status=exc.status_code)
+                raise exc
+
+        logger.info('Successfully created/registered new account against share surveys email address')
+        return render_template('register/register-share-survey-registration-complete.html')
+
+    else:
+        return render_template('register/register-share-survey-enter-your-details.html', form=form, errors=form.errors,
+                               email=email)
--- tests/integration/views/account/test_accept_share_survey.py
+++ tests/integration/views/account/test_accept_share_survey.py
@@ -18,6 +18,9 @@ url_get_business_details = f"{app.config['PARTY_URL']}/party-api/v1/businesses"
 url_get_user_count = f"{app.config['PARTY_URL']}/party-api/v1/share-survey-users-count?business_id={business_party['id']}&survey_id={'02b9c366-7397-42f7-942a-76dc5876d86d'}"
 url_post_pending_shares = f"{app.config['PARTY_URL']}/party-api/v1/pending-shares"
 url_get_survey_second = f"{app.config['SURVEY_URL']}/surveys/02b9c366-7397-42f7-942a-76dc5876d86d"
+url_get_pending_shares_by_batch = f"{app.config['PARTY_URL']}/party-api/v1/share-survey/{batch_number}"
+url_get_shared_to_respondent_party = f"{app.config['PARTY_URL']}/party-api/v1/respondents/email"
+url_post_share_survey_respondent = f"{app.config['PARTY_URL']}/party-api/v1/share-survey-respondent"
 dummy_business = {'associations': [{'businessRespondentStatus': 'ACTIVE',
                                     'enrolments': [{'enrolmentStatus': 'ENABLED',
                                                     'surveyId': '02b9c366-7397-42f7-942a-76dc5876d86d'}],
@@ -99,7 +102,7 @@ class TestAcceptShareSurvey(unittest.TestCase):
 
     @requests_mock.mock()
     @patch('frontstage.controllers.party_controller.get_survey_list_details_for_party')
-    def test_get_accept_share_surveys_success(self, mock_request, get_survey_list):
+    def test_get_accept_share_surveys_success_existing_account(self, mock_request, get_survey_list):
         survey_list = [{"case_id": "6f698975-0a36-45ff-ba66-7a575e414023",
                         "status": "Not started", "collection_instrument_type": "EQ",
                         "survey_id": "02b9c366-7397-42f7-942a-76dc5876d86d",
@@ -116,9 +119,62 @@ class TestAcceptShareSurvey(unittest.TestCase):
         mock_request.get(url_get_survey, status_code=200, json=survey)
         mock_request.get(url_get_survey_second, status_code=200, json=dummy_survey)
         mock_request.post(url_post_accept_share_survey, status_code=201)
+        mock_request.get(url_get_pending_shares_by_batch, status_code=200, json=[dummy_pending_share])
+        mock_request.get(url_get_shared_to_respondent_party, status_code=200, json={})
         get_survey_list.return_value = survey_list
         response = self.app.get(f'/my-account/confirm-share-surveys/{batch_number}', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
+
+    @requests_mock.mock()
+    @patch('frontstage.controllers.party_controller.get_survey_list_details_for_party')
+    def test_get_accept_share_surveys_success_non_existing_account(self, mock_request, get_survey_list):
+        survey_list = [{"case_id": "6f698975-0a36-45ff-ba66-7a575e414023",
+                        "status": "Not started", "collection_instrument_type": "EQ",
+                        "survey_id": "02b9c366-7397-42f7-942a-76dc5876d86d",
+                        "survey_long_name": "Quarterly Business Survey",
+                        "survey_short_name": "QBS", "survey_ref": "139",
+                        "business_party_id": "44d8db36-2319-41c6-8887-79033ce55a4b",
+                        "business_name": "PC UNIVERSE", "trading_as": "PC LTD",
+                        "business_ref": "49900000007", "period": "December 2019",
+                        "submit_by": "26 Mar 2021", "collection_exercise_ref": "1912",
+                        "added_survey": None, "display_button": True}]
+        mock_request.get(url_banner_api, status_code=404)
+        mock_request.get(url_get_respondent_party, status_code=200, json=respondent_party)
+        mock_request.get(url_get_business_details, status_code=200, json=[dummy_business])
+        mock_request.get(url_get_survey, status_code=200, json=survey)
+        mock_request.get(url_get_survey_second, status_code=200, json=dummy_survey)
+        mock_request.post(url_post_accept_share_survey, status_code=201)
+        mock_request.get(url_get_pending_shares_by_batch, status_code=200, json=[dummy_pending_share])
+        mock_request.get(url_get_shared_to_respondent_party, status_code=404)
+        get_survey_list.return_value = survey_list
+        response = self.app.get(f'/my-account/confirm-share-surveys/{batch_number}', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("Your user account will be deleted after 36 months of inactivity".encode(), response.data)
+        self.assertIn("Create a password".encode(), response.data)
+        self.assertIn("Create a password".encode(), response.data)
+
+    @requests_mock.mock()
+    def test_post_accept_share_surveys_success_non_existing_account(self, mock_request):
+        mock_request.get(url_banner_api, status_code=404)
+        mock_request.post(url_post_accept_share_survey, status_code=201)
+        mock_request.get(url_get_pending_shares_by_batch, status_code=200, json=[dummy_pending_share])
+        mock_request.post(url_post_share_survey_respondent, status_code=201, json={})
+        data = {
+            'first_name': 'test',
+            'last_name': 'test',
+            'password': 'Test!129',
+            'password_confirm': 'Test!129',
+            'phone_number': '07456534567',
+        }
+
+        response = self.app.post(f'/register/share-surveys/create-account/enter-account-details?batch_no={batch_number}'
+                                 f'&email=test@test.com',
+                                 data=data,
+                                 follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("Thank you".encode(), response.data)
+        self.assertIn("You are now registered to access the business survey/s shared with you.".encode(), response.data)
+        self.assertIn("To access your surveys you must first sign in.".encode(), response.data)
 
     @requests_mock.mock()
     def test_get_accept_share_surveys_fail(self, mock_request):

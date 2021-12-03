ce23bbfde14535049c2c65d01f4c5ac603874048
Adam Mann <manny160389@hotmail.com>
2021-12-03 15:50:55 +0000

    Prototype redis cache

---
+++ frontstage/common/redis_cache.py
@@ -0,0 +1,35 @@
+import json
+import logging
+
+from flask import current_app as app
+from structlog import wrap_logger
+
+from frontstage import redis
+from frontstage.controllers.survey_controller import get_survey
+
+logger = wrap_logger(logging.getLogger(__name__))
+
+
+class RedisCache:
+    def get(self, key):
+        """
+        Gets the key from redis
+
+        :param key: Key in redis (for this example will be a frontstage:survey id)
+        :return:
+        """
+        redis_key = f"frontstage:{key}"
+        logger.info("Seeing if it's in the cache", key=key, redis_key=redis_key)
+        result = redis.get(redis_key)
+        if not result:
+            logger.info("Result not in cache", key=key, redis_key=redis_key)
+            result = get_survey(app.config["SURVEY_URL"], app.config["BASIC_AUTH"], key)
+            self.save(redis_key, result)
+            return result
+        logger.info("It's in the cache, returning", key=key)
+        return json.loads(result.decode("utf-8"))
+
+    def save(self, key, value):
+        logger.info("Saving result to cache")
+        # TODO make expiry configurable,
+        redis.set(key, json.dumps(value), ex=60)

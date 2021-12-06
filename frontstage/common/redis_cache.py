76c0d6212cd7bd6747d5cd36d8878e58ee70c4a4
Adam Mann <manny160389@hotmail.com>
2021-12-06 16:36:51 +0000

    Small changes

--- frontstage/common/redis_cache.py
+++ frontstage/common/redis_cache.py
@@ -6,11 +6,14 @@ from structlog import wrap_logger
 
 from frontstage import redis
 from frontstage.controllers.survey_controller import get_survey
+from frontstage.exceptions.exceptions import
 
 logger = wrap_logger(logging.getLogger(__name__))
 
 
 class RedisCache:
+    SURVEY_CATEGORY_EXPIRY = 60
+
     def get(self, category, key):
         """
         Gets the key from redis
@@ -20,17 +23,22 @@ class RedisCache:
         :return:
         """
         redis_key = f"frontstage:{category}:{key}"
-        logger.info("Seeing if it's in the cache", key=key, redis_key=redis_key)
+        logger.info("Finding key in the cache", key=key, redis_key=redis_key)
         result = redis.get(redis_key)
         if not result:
             logger.info("Result not in cache", key=key, redis_key=redis_key)
-            result = get_survey(app.config["SURVEY_URL"], app.config["BASIC_AUTH"], key)
+            if category == "survey":
+                result = get_survey(app.config["SURVEY_URL"], app.config["BASIC_AUTH"], key)
+            else:
+                raise KeyError
+                raise ValueError(f"Category [{category}] is not supported")
             self.save(redis_key, result)
             return result
-        logger.info("It's in the cache, returning", key=key)
+
+        logger.info("Key was found in the cache", key=key)
         return json.loads(result.decode("utf-8"))
 
     def save(self, key, value):
         logger.info("Saving result to cache")
         # TODO make expiry configurable,
-        redis.set(key, json.dumps(value), ex=60)
+        redis.set(key, json.dumps(value), ex=self.SURVEY_CATEGORY_EXPIRY)

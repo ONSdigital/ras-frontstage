a4ba45d2e2eaa25a99496a3f93ddc59b76a56b60
Adam Mann <manny160389@hotmail.com>
2022-01-13 11:25:29 +0000

    First pass

--- frontstage/common/encrypter.py
+++ frontstage/common/encrypter.py
@@ -12,11 +12,14 @@ class Encrypter:
         validate_required_keys(keys, KEY_PURPOSE)
         self.key_store = KeyStore(keys)
 
-    def encrypt(self, payload):
+    def encrypt(self, payload, service):
         """
         Encrypts the payload using the keystore values
         :param payload: the value to encrypt
+        :param service: The target service, needed to know which key in the keystore to use.
         :return: string of encrypted data
         """
-        encrypted_data = encrypt(payload, key_store=self.key_store, key_purpose=KEY_PURPOSE)
+        encrypted_data = encrypt(
+            payload, key_store=self.key_store, key_purpose=KEY_PURPOSE, encryption_for_service=service
+        )
         return encrypted_data

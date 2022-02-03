ad496587d73d2583903bcf229f212ee959b18a07
Amit Sinha <amit@creativeappdevs.com>
2022-02-03 10:01:59 +0000

    review comment fix

--- config.py
+++ config.py
@@ -89,7 +89,7 @@ class DevelopmentConfig(Config):
 
     ACCOUNT_SERVICE_URL = os.getenv("ACCOUNT_SERVICE_URL", "http://localhost:8082/surveys/todo")
     ACCOUNT_SERVICE_LOG_OUT_URL = os.getenv("ACCOUNT_SERVICE_LOG_OUT_URL", "http://localhost:8082/sign-in/logout")
-    EQ_URL = os.getenv("EQ_URL", "http://localhost:8086/complete-eq?token=")
+    EQ_URL = os.getenv("EQ_URL", "http://localhost:8086/session?token=")
     EQ_V3_URL = os.getenv("EQ_V3_URL", "http://localhost:8086/v3/session?token=")
     JSON_SECRET_KEYS = os.getenv("JSON_SECRET_KEYS") or open("./tests/test_data/jwt-test-keys/test_key.json").read()
 

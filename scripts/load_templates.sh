0e02a8527a38dde0e0eb0214076d22d7305a4739
Richard McCarthy <richard.mccarthy.ons@gmail.com>
2022-03-08 18:39:19 +0000

    update usage message in load templates

--- scripts/load_templates.sh
+++ scripts/load_templates.sh
@@ -5,7 +5,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "${DIR}"/.. || exit
 
 if [ $# -eq 0 ] || [ "$1" == "" ]; then
-    echo "Usage: load_release.sh Design System {TAG_NAME}"
+    echo "Usage: load_release.sh {DESIGN_SYSTEM_VERSION}"
 else
     DESIGN_SYSTEM_VERSION="$1"
 fi

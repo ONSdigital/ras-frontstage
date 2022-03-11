fae667fb7b5bc3abd38212134a86c4f285100b26
Alejandro Arroyo <alejandro.arroyo@ons.gov.uk>
2022-03-11 15:12:45 +0000

    handling no verification tokens present

--- frontstage/views/passwords/reset_password.py
+++ frontstage/views/passwords/reset_password.py
@@ -25,9 +25,13 @@ def get_reset_password(token, form_errors=None):
         _ = verification.decode_email_token(token, duration)
         email = verification.decode_email_token(token, duration)
         respondent = party_controller.get_respondent_by_email(email)
-        if token not in respondent["verification_tokens"]:
+        try:
+            tokens = respondent["verification_tokens"]
+        except KeyError:
+            tokens = []
+        if token not in tokens:
             logger.warning("Token not found for respondent", token=token, respondent_id=respondent["id"])
-            raise BadData("Token not found for respondent")
+            return render_template("passwords/password-token-not-found.html", token=token)
     except SignatureExpired:
         logger.warning("Token expired for frontstage reset", token=token, exc_info=True)
         return render_template("passwords/password-expired.html", token=token)

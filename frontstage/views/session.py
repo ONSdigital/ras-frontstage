c5bc42e0eddd02d2cb13fa1f9f8a236a8040582a
SteveScorfield <steve.scorfield@ons.gov.uk>
2023-05-09 12:38:49 +0100

    Updated the code to ensure that the timeout redirect doesnt cause the signin to goto the session expiry endpoint

--- frontstage/views/session.py
+++ frontstage/views/session.py
@@ -1,13 +1,28 @@
-from datetime import datetime, timezone
-
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, make_response, redirect, request, session, url_for
 
 from frontstage.common.authorisation import jwt_authorization
+from frontstage.common.session import Session
 
 session_bp = Blueprint("session", __name__)
 
 
 @session_bp.route("/expires-at", methods=["GET", "PATCH"])
 @jwt_authorization(request, refresh_session=False)
-def session_expires_at(session):
-    return jsonify(expires_at=datetime.fromtimestamp(session.get_expires_in(), tz=timezone.utc).isoformat())
+def session_expires_at(expiry_session):
+    return jsonify(expires_at=expiry_session.get_formatted_expires_in())
+
+
+@session_bp.route("/session-expired", methods=["GET"])
+def session_expired():
+    # Delete user session in redis
+    session_key = request.cookies.get("authorization")
+    current_session = Session.from_session_key(session_key)
+    current_session.delete_session()
+    # We need to remove the next value from the flask session otherwise, user is redirected to the expiry end point and
+    # Not the survey list page
+    if session.get("next"):
+        session.pop("next")
+    # Delete session cookie
+    response = make_response(redirect(url_for("sign_in_bp.login")))
+    response.delete_cookie("authorization")
+    return response

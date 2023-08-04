1963122f949e787aea7391cb4d7b739ae36e25de
LJBabbage <ljbabbage@hotmail.com>
2023-08-04 14:46:31 +0100

    RAS-859 Frontstage logging not thread save

--- frontstage/logger_config.py
+++ frontstage/logger_config.py
@@ -1,59 +1,45 @@
 import logging
 import sys
-
-from structlog import configure
-from structlog.processors import JSONRenderer, TimeStamper, format_exc_info
-from structlog.stdlib import LoggerFactory, add_log_level, filter_by_level
-from structlog.threadlocal import wrap_dict
+import structlog
 
 
-def logger_initial_config(log_level="INFO", logger_format="%(message)s", logger_date_format="%Y-%m-%dT%H:%M%s"):
-    def add_service(logger, method_name, event_dict):
+def logger_initial_config(log_level: str = "INFO") -> None:
+    def add_severity_level(_, method_name: str, event_dict: dict) -> dict:
         """
-        Add the service name to the event dict.
-        This adds `service: 'ras-frontstage'` to all log lines.
+        Adds the log level to the event dict.
+        """
+        event_dict["severity"] = method_name
+        return event_dict
+
+    def add_service(_, __, event_dict: dict) -> dict:
+        """
+        Adds the service name (ras-frontstage) to the event dict.
         """
         event_dict["service"] = "ras-frontstage"
         return event_dict
 
-    logging.basicConfig(stream=sys.stdout, level=log_level, format=logger_format)
-    auth_log = logging.getLogger(__name__)
-    auth_log.addHandler(logging.NullHandler())
-    auth_log.propagate = False
-
-    def add_severity_level(logger, method_name, event_dict):
+    def parse_exception(_, __, event_dict: dict) -> dict:
         """
-        Add the log level to the event dict.
+        Formats the exception string in the event_dict.
         """
-        if method_name == "warn":
-            # The stdlib has an alias
-            method_name = "warning"
-
-        event_dict["severity"] = method_name
-        return event_dict
-
-    def parse_exception(_, __, event_dict):
         exception = event_dict.get("exception")
         if exception:
             event_dict["exception"] = exception.replace('"', "'").split("\n")
         return event_dict
 
-    # setup file logging
-    renderer_processor = JSONRenderer(indent=None)
+    logging.basicConfig(format="%(message)s", stream=sys.stdout, level=logging.INFO)
 
-    processors = [
-        add_severity_level,
-        add_log_level,
-        filter_by_level,
-        add_service,
-        format_exc_info,
-        TimeStamper(fmt=logger_date_format, utc=True, key="created_at"),
-        parse_exception,
-        renderer_processor,
-    ]
-    configure(
-        context_class=wrap_dict(dict),
-        logger_factory=LoggerFactory(),
-        processors=processors,
+    structlog.configure(
+        processors=[
+            structlog.contextvars.merge_contextvars,
+            add_severity_level,
+            add_service,
+            structlog.processors.format_exc_info,
+            structlog.processors.TimeStamper(fmt="%Y-%m-%dT%H:%M%s", utc=True, key="created_at"),
+            parse_exception,
+            structlog.processors.JSONRenderer(indent=None),
+        ],
+        wrapper_class=structlog.make_filtering_bound_logger(logging.getLevelName(log_level)),
+        logger_factory=structlog.stdlib.LoggerFactory(),
         cache_logger_on_first_use=True,
     )

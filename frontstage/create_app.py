24acdbc413d054aa2a140c07927c1110a0882b86
LJBabbage <ljbabbage@hotmail.com>
2023-09-18 14:10:53 +0100

    Add profiler code

--- frontstage/create_app.py
+++ frontstage/create_app.py
@@ -1,6 +1,7 @@
 import logging
 import os
 
+import googlecloudprofiler
 from flask import Flask, request
 from flask_wtf.csrf import CSRFProtect
 from structlog import wrap_logger
@@ -56,6 +57,21 @@ def create_app_object():
 
     csrf = CSRFProtect(app)
     csrf.exempt("frontstage.views.session.session_refresh_expires_at")
+
+    # Profiler initialization. It starts a daemon thread which continuously
+    # collects and uploads profiles. Best done as early as possible.
+    try:
+        googlecloudprofiler.start(
+            service="hello-profiler",
+            service_version="1.0.1",
+            # verbose is the logging level. 0-error, 1-warning, 2-info,
+            # 3-debug. It defaults to 0 (error) if not set.
+            verbose=3,
+            # project_id must be set if not running on GCP.
+            # project_id='my-project-id',
+        )
+    except (ValueError, NotImplementedError) as exc:
+        print(exc)  # Handle errors here
 
     @app.after_request
     def apply_headers(response):

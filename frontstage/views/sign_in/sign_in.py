e85c2f051827fd34ce6b7ef595fe556ef37a0be0
SteveScorfield <steve.scorfield@ons.gov.uk>
2023-10-17 12:25:21 +0100

    Have removed profiler when testing config is present

--- frontstage/views/sign_in/sign_in.py
+++ frontstage/views/sign_in/sign_in.py
@@ -1,4 +1,5 @@
 import logging
+import os
 from os import getenv
 
 import googlecloudprofiler
@@ -37,10 +38,11 @@ def home():
 
 @sign_in_bp.route("/", methods=["GET", "POST"])
 def login():  # noqa: C901
-    try:
-        googlecloudprofiler.start(verbose=3, service="frontstage")
-    except (ValueError, NotImplementedError) as exc:
-        print(exc)  # Handle errors here
+    if "TestingConfig" not in os.environ["APP_SETTINGS"]:
+        try:
+            googlecloudprofiler.start(verbose=3, service="frontstage")
+        except (ValueError, NotImplementedError) as exc:
+            print(exc)  # Handle errors here
 
     form = LoginForm(request.form)
     if form.username.data is not None:

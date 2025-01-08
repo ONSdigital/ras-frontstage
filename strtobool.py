45b62df6df21dd905f3bd4d2f97e67626c334671
SteveScorfield <steve.scorfield@ons.gov.uk>
2025-01-08 11:09:53 +0000

    Removed comment

--- strtobool.py
+++ strtobool.py
@@ -1,8 +1,4 @@
 def strtobool(val):
-    """This directly replaces the distutils function strtobool. All the redundant checks that were carried out
-    originally by the function are no longer needed. We either need a true or false value. We don't need to compare if
-    there is a I/O, 1/0, Yes/No, Y/N or t/f etc.
-    """
     val = val.lower()
     if val == "true":
         return True

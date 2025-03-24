28079eb6a36747d6280f00b940eaf103e67a45da
matthew-robinson-ons <matthew.robinson@ons.gov.uk>
2025-03-24 09:35:38 +0000

    refactor view

--- frontstage/views/help.py
+++ frontstage/views/help.py
@@ -1,49 +1,20 @@
 import logging
 
-from flask import Blueprint, flash, request, url_for
+from flask import Blueprint, flash, request
 from structlog import wrap_logger
-from werkzeug.utils import redirect
 
-from frontstage.models import HelpForm, HelpInfoOnsForm, HelpPasswordForm
+from frontstage.models import HelpInfoOnsForm
 from frontstage.views.template_helper import render_template
 
 logger = wrap_logger(logging.getLogger(__name__))
 
 help_bp = Blueprint("help_bp", __name__, static_folder="static", template_folder="templates")
 
-form_redirect_mapper = {
-    "info-ons": "help_bp.info_ons_page",
-    "password": "help_bp.help_with_password_page",
-    "something-else": "help_bp.something_else",
-}
 form_render_page_mapper = {
     "ons": "help/help-who-is-ons.html",
     "data": "help/help-info-how-safe-data.html",
     "info-something-else": "help/help-info-something-else.html",
-    "reset-email": "help/help-with-password-reset-email.html",
-    "password-not-accept": "help/help-with-password-not-accept.html",
-    "reset-password": "help/help-with-password-reset.html",
-    "password-something-else": "help/help-with-password-something-else.html",
 }
-
-
-@help_bp.route("/vulnerability-reporting", methods=["GET"])
-def help():
-    return render_template("vunerability_disclosure.html")
-
-
-@help_bp.route("/", methods=["GET", "POST"])
-def help_page():
-    page_title = "Help"
-    form = HelpForm(request.values)
-    if request.method == "POST":
-        if form.validate():
-            return redirect(url_for(form_redirect_mapper.get(form.data["option"])))
-        else:
-            flash("At least one option should be selected.")
-            page_title = "Error: " + page_title
-
-    return render_template("help/help.html", form=HelpForm(), page_title=page_title)
 
 
 @help_bp.route("/info-ons", methods=["GET", "POST"])
@@ -58,22 +29,3 @@ def info_ons_page():
             page_title = "Error: " + page_title
 
     return render_template("help/help-info-ons.html", form=HelpInfoOnsForm(), page_title=page_title)
-
-
-@help_bp.route("/help-with-my-password", methods=["GET", "POST"])
-def help_with_password_page():
-    page_title = "Help with my password"
-    form = HelpPasswordForm(request.values)
-    if request.method == "POST":
-        if form.validate():
-            return render_template(form_render_page_mapper.get(form.data["option"]))
-        else:
-            flash("At least one option should be selected.")
-            page_title = "Error: " + page_title
-
-    return render_template("help/help-with-password.html", form=HelpPasswordForm(), page_title=page_title)
-
-
-@help_bp.route("/something-else", methods=["GET"])
-def something_else():
-    return render_template("help/help-something-else.html")
--- tests/integration/test_sign_out_help.py
+++ tests/integration/test_sign_out_help.py
@@ -12,20 +12,6 @@ class TestSignOutHelp(unittest.TestCase):
         self.app.set_cookie("authorization", "session_key")
 
     @requests_mock.mock()
-    def test_sign_out_help_post(self, mock_request):
-        mock_request.get(url_banner_api, status_code=404)
-        form = {"option": "info-ons"}
-        response = self.app.post("/help", data=form, follow_redirects=True)
-        self.assertEqual(response.status_code, 200)
-        self.assertIn("Information about the Office for National Statistics (ONS)".encode(), response.data)
-        self.assertIn("Choose an option".encode(), response.data)
-        self.assertIn("Who is the Office for National Statistics (ONS)?".encode(), response.data)
-        self.assertIn("How do you keep my data safe?".encode(), response.data)
-        self.assertIn("Something else".encode(), response.data)
-        self.assertIn("Continue".encode(), response.data)
-        self.assertIn("Cancel".encode(), response.data)
-
-    @requests_mock.mock()
     def test_sign_out_help_post_ons_info_option_needs_to_be_selected(self, mock_request):
         mock_request.get(url_banner_api, status_code=404)
         form = {}

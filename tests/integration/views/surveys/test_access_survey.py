106765daa4108aad15ed15898e0275f1ebb3a3ea
SteveScorfield <steve.scorfield@ons.gov.uk>
2025-08-04 10:12:14 +0100

    Fix last test

--- tests/integration/views/surveys/test_access_survey.py
+++ tests/integration/views/surveys/test_access_survey.py
@@ -194,7 +194,7 @@ class TestAccessSurvey(unittest.TestCase):
 
     @patch("frontstage.controllers.party_controller.is_respondent_enrolled")
     @patch("frontstage.controllers.party_controller.RedisCache.get_registry_instrument")
-    def test_generate_eq_url(self, mock_request, _, mock_cache):
+    def test_generate_eq_url(self, mock_request, mock_cache, _):
         # Given all external services are mocked, and we have an EQ collection instrument
         mock_request.get(url_get_case, json=case)
         mock_request.get(url_get_collection_exercise, json=collection_exercise_v3)
@@ -206,6 +206,7 @@ class TestAccessSurvey(unittest.TestCase):
         mock_request.post(url_post_case_event_uuid, status_code=201)
         mock_request.get(url_get_respondent_party, status_code=200, json=respondent_party)
         mock_request.get(url_banner_api, status_code=404)
+        mock_cache.return_value = None
 
         # When the generate-eq-url is called
         response = self.app.get(

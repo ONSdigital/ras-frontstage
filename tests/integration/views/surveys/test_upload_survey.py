75184003923fb1253dc72fdd2ce8b206a724e034
SteveScorfield <steve.scorfield@ons.gov.uk>
2026-09-04 11:09:45 +0100

    Fix lint

--- tests/integration/views/surveys/test_upload_survey.py
+++ tests/integration/views/surveys/test_upload_survey.py
@@ -31,7 +31,8 @@ class TestUploadSurvey(unittest.TestCase):
         self.app = app.test_client()
         self.app.set_cookie("authorization", "session_key")
         self.headers = {
-            "Authorization": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoicmluZ3JhbUBub3d3aGVyZS5jb20iLCJ1c2VyX3Njb3BlcyI6WyJjaS5yZWFkIiwiY2kud3JpdGUiXX0.se0BJtNksVtk14aqjp7SvnXzRbEKoqXb8Q5U9VVdy54"
+            "Authorization": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoicmluZ3JhbUBub3d3aGVyZS5jb20iLCJ1c2Vy"
+                             + "X3Njb3BlcyI6WyJjaS5yZWFkIiwiY2kud3JpdGUiXX0.se0BJtNksVtk14aqjp7SvnXzRbEKoqXb8Q5U9VVdy54"
             # NOQA
         }
         self.survey_file = dict(file=(io.BytesIO(b"my file contents"), "testfile.xlsx"))
